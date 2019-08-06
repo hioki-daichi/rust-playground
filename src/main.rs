062d396b94a85cb8b80439727a9ae4a4b9ee1b41
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-06 21:20:36 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#[derive(Debug)]
+struct Parent(usize, Child, Child);
+
+#[derive(Debug)]
+struct Child(usize);
+
+fn f1(p: &Parent) {
+    println!("p {:?}", p);
+}
+
+fn f2(p: &mut Parent) {
+    println!("p {:?}", p);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut p = Parent(1, Child(11), Child(12));
+    f1(&p);
+    f2(&mut p);
+    println!("p {:?}", p)
 }
