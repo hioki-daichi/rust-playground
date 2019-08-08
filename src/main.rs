1f05d9c125c8b0e69184eb8a9838309908b415d4
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-08 21:25:14 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,12 @@
-use std::collections::HashMap;
-
-// get_mut と insert の 2 つのメソッドで map に対して可変の借用をとっている。
-// 可変の借用が 2 つ同時に存在することは許されないため、借用のライフタイムが重ならないようにしなけ
-// ればならない。
-fn process_or_default(key: char, map: &mut HashMap<char, String>) {
-    // get_mut は Option<&mut String> を返す。
-    // この戻り値(可変の参照)が生存している間は map の可変の借用が有効。
-    match map.get_mut(&key) {
-        // value を戻り値に束縛しているためこの腕の中まで map の可変の借用が有効になる。
-        Some(value) => value.push_str(", world!"),
-        // None 側は何も束縛していないため get_mut による可変の借用は終了している
-        None => {
-            map.insert(key, Default::default()); // 再び insert で可変の借用をとれる
-        }
-    }
-}
-
 fn main() {
-    let mut map = HashMap::new();
-    map.insert('h', "Hello".to_string());
-    process_or_default('h', &mut map);
-    println!("{:?}", map); //=> {'h': "Hello, world!"}
+    let c1 = 'A';
+    let c1_ptr: &char = &c1; // &char 型。不変の参照。
+    assert_eq!(*c1_ptr, 'A');
+
+    let mut n1 = 0;
+    let n1_ptr: &mut i32 = &mut n1; // &mut i32 型。可変の参照。
+    assert_eq!(*n1_ptr, 0);
+
+    *n1_ptr = 1_000;
+    assert_eq!(*n1_ptr, 1_000);
 }
