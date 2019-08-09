c2dd95e71420dbe3a8f15f4b1d1ecef499e0cbfe
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-09 23:14:03 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 fn main() {
-    let s = "foo\nbar\nbaz";
-    let mut lines = s.lines();
-    assert_eq!(lines.next(), Some("foo"));
-    assert_eq!(lines.next(), Some("bar"));
-    assert_eq!(lines.next(), Some("baz"));
+    let a = "aあbい";
+    assert_eq!(a.get(0..1), Some("a"));
+    assert_eq!(a.get(1..3), None);
+    assert_eq!(a.get(1..4), Some("あ"));
+    assert_eq!(a.get(4..5), Some("b"));
+    assert_eq!(a.get(5..8), Some("い"));
+    assert_eq!(a.get(5..9), None);
 }
