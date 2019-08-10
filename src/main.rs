7ecfaba1dc15465e2497d644c866de1925cb37c9
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-10 23:33:02 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 fn main() {
-    let v1: Vec<u8> = vec![b'h', b'e', b'l', b'l', b'o'];
-    let v2: Vec<u16> = v1.iter().map(|&n| n as u16).collect::<Vec<u16>>();
-    println!("{:?}", v2) // [104, 101, 108, 108, 111]
+    let v1: Vec<u8> = From::from("hello");
+    println!("{:?}", v1); // [104, 101, 108, 108, 111]
 }
