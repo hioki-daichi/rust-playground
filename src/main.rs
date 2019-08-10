ba84f5b273e4e7f33717815656d8d2515f5a700f
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-10 23:18:00 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,5 @@
-#[derive(Default)]
-struct A {
-    f0: u8,
-    f1: u32, // x86_64 アーキテクチャでは i32 は 4 の倍数のアドレスにアラインされていることを要求する
-    f2: u8,
-}
-
 fn main() {
-    let a: A = Default::default();
-    println!(
-        "struct A ({} bytes) \n  \
-         f0: {:p}\n  \
-         f1: {:p}\n  \
-         f2: {:p}\n", // {:p} で格納されているアドレスを表示
-        std::mem::size_of::<A>(),
-        &a.f0,
-        &a.f1,
-        &a.f2
-    );
-    // struct A (8 bytes)
-    //   f0: 0x7ffee06c45f4 // 末尾 4 のため、f1 より後に定義されていることがわかる
-    //   f1: 0x7ffee06c45f0 // 末尾 0 のため、f0 より先に定義されていることがわかる
-    //   f2: 0x7ffee06c45f5
+    let i1: i32 = 10;
+    let f1 = i1 as f64 / 2.5; // i32 型から f64 型へキャスト
+    println!("{}", f1); // 4
 }
