cfe9a39b5282fd0d6123b7fbecd7487467082cfa
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-10 15:21:39 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+fn f1(name: &str) -> String {
+    format!("Hello, {}!", name)
+}
+
 fn main() {
-    // char の並びから str は作れないが String なら作れる。
-    let cs: [char; 4] = ['R', 'u', 's', 't'];
-    let s1: String = cs.iter().collect::<String>();
-    assert_eq!(s1, "Rust");
+    println!("{}", f1("World")); // Hello, World!
 }
