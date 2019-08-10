133ef287398320a46c14b2b40b33581288112191
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-10 22:15:00 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,48 @@
-#[derive(Debug, PartialEq)]
-enum Weekday {
-    Monday,
-    Tuesday,
-    Wednesday,
-    Thursday,
-    Friday,
-}
+type UserName = String;
 
-fn foo(weekday: Weekday) -> String {
-    if weekday == Weekday::Friday {
-        return format!("{:?}!!!", weekday);
-    } else {
-        return format!("still {:?}...", weekday);
-    }
-}
+#[derive(Debug)]
+enum Task {
+    Open,
+    AssignedTo(UserName), // タプル構造体
+    Working {
+        assignee: UserName,
+        remaining_hours: u16,
+    }, // 名前付きフィールド構造体
+    Done,
+} // 4 つのバリアントを定義
+
+use crate::Task::*; // バリアント名を直接書けるようにしている
 
 fn main() {
-    println!("{:?}", foo(Weekday::Monday));
-    println!("{:?}", foo(Weekday::Tuesday));
-    println!("{:?}", foo(Weekday::Wednesday));
-    println!("{:?}", foo(Weekday::Thursday));
-    println!("{:?}", foo(Weekday::Friday));
-    // "still Monday..."
-    // "still Tuesday..."
-    // "still Wednesday..."
-    // "still Thursday..."
-    // "Friday!!!"
+    // Task 型の値を 3 つ作ってベクタに格納している
+    let tasks = vec![
+        AssignedTo(String::from("daichi")),
+        Working {
+            assignee: String::from("tsumugi"),
+            remaining_hours: 1,
+        },
+        Done,
+        Open,
+    ];
+
+    // 個々のタスクの状況をレポートする
+    for (i, task) in tasks.iter().enumerate() {
+        match task {
+            AssignedTo(assignee) => {
+                println!("タスク{}は{}さんにアサインされています", i, assignee);
+            }
+            Working {
+                assignee,
+                remaining_hours,
+            } => println!(
+                "タスク{}は{}さんが作業中です。残り{}時間の見込み",
+                i, assignee, remaining_hours
+            ),
+            _ => println!("タスク{}はその他のステータス({:?})です", i, task),
+        }
+    }
+    // タスク0はdaichiさんにアサインされています
+    // タスク1はtsumugiさんが作業中です。残り1時間の見込み
+    // タスク2はその他のステータス(Done)です
+    // タスク3はその他のステータス(Open)です
 }
