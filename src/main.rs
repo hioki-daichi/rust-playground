7911a7eb05e259d124b21f2d0f9596842b0bbcc9
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-10 13:38:50 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
+use std::collections::HashMap;
+
 fn main() {
-    let mut v1: Vec<i32> = vec![0, 1, 2, 3];
+    let mut m1 = HashMap::new();
 
-    // 余分なメモリを確保する様子
-    println!("capacity: {}", v1.capacity()); // capacity: 4
-    v1.push(4);
-    println!("capacity: {}", v1.capacity()); // capacity: 8
+    m1.insert("a", 1);
+    m1.insert("b", 2);
 
-    // Box<[i32]> に変換すると Vec の shrink_to_fit() が呼ばれて余分なメモリを持たなくなる様子
-    println!("capacity: {}", v1.into_boxed_slice().into_vec().capacity()); // capacity: 5
+    // 要素数は 2
+    assert_eq!(m1.len(), 2);
+
+    // キーに対応する値を取り出す
+    assert_eq!(m1.get("a"), Some(&1));
+    assert_eq!(m1.get("b"), Some(&2));
+    assert_eq!(m1.get("c"), None);
+
+    // "d" が存在するならその値の参照を得る。
+    // 存在しないなら "d" に対して 0 を登録してから参照を返す。
+    let d = m1.entry("d").or_insert(0);
+    assert_eq!(d, &0);
+    *d = 1;
+    assert_eq!(m1.get("d"), Some(&1));
 }
