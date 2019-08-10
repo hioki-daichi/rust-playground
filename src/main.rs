a254ee8f55f2eccd0e0b1412a7b3de58f7ed4c21
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-10 16:15:41 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 fn main() {
-    assert_eq!(.., std::ops::RangeFull);
-    assert_eq!(..3, std::ops::RangeTo { end: 3 });
-    assert_eq!(..=3, std::ops::RangeToInclusive { end: 3 });
-    assert_eq!(1.., std::ops::RangeFrom { start: 1 });
-    assert_eq!(1..3, std::ops::Range { start: 1, end: 3 });
-    assert_eq!(1..=3, std::ops::RangeInclusive::new(1, 3));
+    assert_eq!(
+        Some(300).and_then(|n| if n > 255 { Some(255) } else { Some(n) }),
+        Some(255)
+    );
 }
