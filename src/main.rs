5e2f3560fbf72a0dbc7383308824b9730cd88b4c
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-11 01:48:23 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
-fn f(p: Box<[i32]>) -> i32 {
-    p[0]
-}
-
 fn main() {
-    let xs: [i32; 4] = [1, 2, 3, 4];
+    let a: Box<std::fmt::Debug>;
 
-    let b: Box<[i32; 4]> = Box::new(xs);
+    let b: [i32; 2] = [1, 2];
+    let c: Option<i32> = Some(1);
 
-    // Box<[i32; 4]> から Box<[i32]> へサイズの不定化が行われている
-    assert_eq!(1, f(b));
+    // ある型が Debug トレイトを実装していればその型からトレイトオブジェクトへ型強制できる
+    a = Box::new(b); // Box<[i32; 2]>  -> Box<Debug>
+    a = Box::new(c); // Box<Some<i32>> -> Box<Debug>
 }
