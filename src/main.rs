c54b94f921ee6e96145aa4ad12acd7f935d2c0a2
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-10 23:29:32 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 fn main() {
-    let t1 = ('a', 42);
-    // let t2 = t1 as (u32, u8); // non-primitive cast: `(char, i32)` as `(u32, u8)`    note: an `as` expression can only be used to convert between primitive types. Consider using the `From` trait
-    let t3 = (t1.0 as u32, t1.1 as u8); // 要素を 1 つずつキャスト
-    println!("{:?}", t3); // (97, 42)
+    let v1: Vec<u8> = vec![b'h', b'e', b'l', b'l', b'o'];
+    let v2: Vec<u16> = v1.iter().map(|&n| n as u16).collect::<Vec<u16>>();
+    println!("{:?}", v2) // [104, 101, 108, 108, 111]
 }
