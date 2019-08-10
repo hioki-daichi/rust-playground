617d600f780ce7e4649df155e66e264922b00f9c
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-10 17:05:39 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-fn f(a: &str, b: &str) -> Result<i32, std::num::ParseIntError> {
-    let x = a.parse::<i32>()?;
-    let y = b.parse::<i32>()?;
-    Ok(x + y)
+type UserName = String;
+type Id = i64;
+type Timestamp = i64;
+type User = (Id, UserName, Timestamp);
+
+fn new_user(name: UserName) -> User {
+    (1, name, 5555555555)
 }
 
 fn main() {
-    assert_eq!(f("1", "2"), Ok(3));
-    assert!(f("1", "a").is_err());
+    println!("{:?}", new_user(String::from("daichi"))); // (1, "daichi", 5555555555)
 }
