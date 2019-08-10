7f7639892617ee37c192df34edf7c721e142a274
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-10 19:32:44 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
-struct Triangle(Vertex, Vertex, Vertex);
-struct Vertex(i32, i32);
+#[repr(transparent)]
+#[derive(Debug)]
+struct Id(u64);
+
+#[repr(transparent)]
+#[derive(Debug)]
+struct Name(String);
+
+#[repr(transparent)]
+#[derive(Debug)]
+struct Timestamp(u64);
+
+type User = (Id, Name, Timestamp);
+
+fn new_user(name: Name, id: Id, created: Timestamp) -> User {
+    (id, name, created)
+}
 
 fn main() {
-    let vx0 = Vertex(0, 0);
-    let vx1 = Vertex(3, 0);
-    let triangle = Triangle(vx0, vx1, Vertex(2, 2));
+    let id = Id(1);
+    let created = Timestamp(9999999999);
+    let name = Name(String::from("daichi"));
 
-    assert_eq!((triangle.1).0, 3);
+    let user = new_user(name, id, created);
+
+    println!("{:?}", user);
 }
