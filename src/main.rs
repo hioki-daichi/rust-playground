2c2b2614a3b564bb957711aab3d2e87585a39180
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-10 16:31:56 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
-fn f(xs: &[i32]) -> Option<i32> {
-    let s0 = xs.get(0)?;
-    let s3 = xs.get(2)?;
-    Some(s0 + s3)
-}
-
 fn main() {
-    assert_eq!(f(&[1, 2, 3]), Some(4));
-    assert_eq!(f(&[1, 2]), None);
+    assert_eq!("10".parse::<i32>(), Ok(10));
+
+    let r = "a".parse::<i32>();
+    assert!(r.is_err());
+    println!("{:?}", r); // Err(ParseIntError { kind: InvalidDigit })
 }
