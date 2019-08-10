6d254dd2f23a315ae686600118af1d67e5229ea5
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-10 14:26:21 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,14 @@
-use std::collections::HashMap;
-
 fn main() {
-    let mut m1 = HashMap::new();
+    let mut foo: String = "foo".to_string();
+    let bar: &str = "bar";
 
-    m1.insert("a", 1);
-    m1.insert("b", 2);
+    foo.push_str(bar); // String 型の文字列に &str 型の文字列を追加
 
-    // 要素数は 2
-    assert_eq!(m1.len(), 2);
+    assert_eq!(foo, "foobar");
 
-    // キーに対応する値を取り出す
-    assert_eq!(m1.get("a"), Some(&1));
-    assert_eq!(m1.get("b"), Some(&2));
-    assert_eq!(m1.get("c"), None);
+    let baz: String = String::from("baz");
 
-    // "d" が存在するならその値の参照を得る。
-    // 存在しないなら "d" に対して 0 を登録してから参照を返す。
-    let d = m1.entry("d").or_insert(0);
-    assert_eq!(d, &0);
-    *d = 1;
-    assert_eq!(m1.get("d"), Some(&1));
+    foo.push_str(&baz); // & を付けると型強制という仕組みによって &str へ変換される
+
+    assert_eq!(foo, "foobarbaz");
 }
