3354af1608d123ae9161c6fdd0f251e9641e787d
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-10 19:48:08 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,8 @@
-#[repr(transparent)]
-#[derive(Debug)]
-struct Id(u64);
-
-#[repr(transparent)]
-#[derive(Debug)]
-struct Name(String);
-
-#[repr(transparent)]
-#[derive(Debug)]
-struct Timestamp(u64);
-
-type User = (Id, Name, Timestamp);
-
-fn new_user(name: Name, id: Id, created: Timestamp) -> User {
-    (id, name, created)
-}
+#[derive(Debug, PartialEq)]
+struct UniqueValue();
 
 fn main() {
-    let id = Id(1);
-    let created = Timestamp(9999999999);
-    let name = Name(String::from("daichi"));
-
-    let user = new_user(name, id, created);
-
-    println!("{:?}", user);
+    let a = UniqueValue();
+    let b = UniqueValue();
+    assert_eq!(a, b);
 }
