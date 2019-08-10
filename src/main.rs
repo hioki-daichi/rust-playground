5af04e3b3f719be4273f3a92daf5e64dd88f4d6f
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-10 19:06:30 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,10 @@
-#[derive(Debug)]
-struct Polygon {
-    vertexes: Vec<(i32, i32)>, // 頂点の座標
-    stroke_width: u8,          // 輪郭の太さ
-    fill: (u8, u8, u8),        // 塗りつぶしの RGB 色
-}
-
-impl Default for Polygon {
-    fn default() -> Self {
-        Self {
-            stroke_width: 1,
-            vertexes: Default::default(),
-            fill: Default::default(),
-        }
-    }
-}
+struct Triangle(Vertex, Vertex, Vertex);
+struct Vertex(i32, i32);
 
 fn main() {
-    let p = Polygon {
-        ..Default::default()
-    };
+    let vx0 = Vertex(0, 0);
+    let vx1 = Vertex(3, 0);
+    let triangle = Triangle(vx0, vx1, Vertex(2, 2));
 
-    println!("{:?}", p); // Polygon { vertexes: [], stroke_width: 1, fill: (0, 0, 0) }
+    assert_eq!((triangle.1).0, 3);
 }
