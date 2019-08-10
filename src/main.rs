4bb2e9747865dee979092ec6416e3cf460f1e965
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-10 14:35:42 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 fn main() {
-    let f: f64 = 4.3 + 0.1;
-    assert_eq!(f.to_string(), "4.3999999999999995");
-    assert_eq!(format!("{:.2}", f), "4.40");
+    let s1 = "42";
+    assert_eq!(s1.parse::<i32>(), Ok(42));
 }
