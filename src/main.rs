b1dcd34c254ae0410e3b0ad1b7b51c21757427f3
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-10 16:29:49 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+fn f(xs: &[i32]) -> Option<i32> {
+    let s0 = xs.get(0)?;
+    let s3 = xs.get(2)?;
+    Some(s0 + s3)
+}
+
 fn main() {
-    assert_eq!(
-        Some(300).and_then(|n| if n > 255 { Some(255) } else { Some(n) }),
-        Some(255)
-    );
+    assert_eq!(f(&[1, 2, 3]), Some(4));
+    assert_eq!(f(&[1, 2]), None);
 }
