f10e98a32de08b95f96e6be7934170e9c46003f8
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-10 23:25:38 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    let a: i32 = 300;
-    let b = a as u8;
-    println!("{}", b) // 44
-                      // 300 は u8 型の最大値を超えているため 300 を 256 で割った余りの 44 になっている
+    let t1 = ('a', 42);
+    // let t2 = t1 as (u32, u8); // non-primitive cast: `(char, i32)` as `(u32, u8)`    note: an `as` expression can only be used to convert between primitive types. Consider using the `From` trait
+    let t3 = (t1.0 as u32, t1.1 as u8); // 要素を 1 つずつキャスト
+    println!("{:?}", t3); // (97, 42)
 }
