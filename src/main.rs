d9c93fafc3eb5f489abcd6f744de4abc119b80ec
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-11 13:48:15 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 fn main() {
-    let o1 = Some("Apple");
+    let mut counter = 10;
 
-    // 複合的な型 (e.g. 列挙型, 構造体, タプル, 参照)
-    // の値からその中の値を取り出すことを「分配束縛」と呼ぶ。
-    let s1 = match o1 {
-        // o1 がバリアント Option::Some の場合に、
-        // 変数 s2 をバリアントに関連付けられている値 ("Apple") に束縛している。
-        Some(s2) => String::from("Hi, ") + s2,
-        None => String::from("Nothing"),
+    let a = loop {
+        if counter == 0 {
+            break counter;
+        }
+
+        counter -= 1;
     };
 
-    println!("{}", s1); // Hi, Apple
+    assert_eq!(a, 0);
 }
