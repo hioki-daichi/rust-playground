557abcabc43d9361da70bbaefac69b54bef29418
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-11 14:50:59 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,5 @@
-#[allow(unused_imports)]
-// rand は外部のクレートなので絶対指定。
-// rand クレートの prelude モジュールの中のすべてのアイテムを使っている。
-use rand::prelude::*;
-
-mod network {
-    pub fn ping() {
-        println!("Ping");
-    }
-}
+mod network;
 
 fn main() {
-    // ルートモジュールからの相対指定
     network::ping(); // Ping
-
-    // 明示的にクレートのルートモジュールを表す crate を書いてもよい
-    crate::network::ping(); // Ping
-
-    // self は現在地のモジュールを指すが、現在地はルートモジュールなので次のようにも書ける
-    self::network::ping(); // Ping
 }
