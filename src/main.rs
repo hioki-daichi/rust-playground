d994a20954f2a83861a2595b64e910ad4f39160c
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-11 13:15:32 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-static mut V: Option<Vec<i32>> = None;
-
 fn main() {
-    unsafe {
-        V = Some(vec![1]);
-        println!("{:?}", V); // Some([1])
-    }
+    let a = 0b11110000;
+    let b = 0b01010000;
+    let c = 0b00001010;
+
+    println!("{:08b}", a & b); // 01010000
+    println!("{:08b}", a & b | c); // 01011010
 }
