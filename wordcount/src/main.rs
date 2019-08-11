b2c36d901737db6b1a49f688c35f7f8dec090745
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-11 20:35:28 +0900

    Update

--- wordcount/src/main.rs
+++ wordcount/src/main.rs
@@ -14,8 +14,8 @@ fn main() {
     let filename = get_filename();
     let file = File::open(filename).expect(ERR_FILE_OPEN);
     let reader = BufReader::new(&file);
-    let freqs = count(reader);
-    println!("{:?}", freqs);
+    let frequency = calculate_frequency(reader);
+    println!("{:?}", frequency);
 }
 
 fn get_filename() -> String {
@@ -24,17 +24,14 @@ fn get_filename() -> String {
     return filename.expect(ERR_FILENAME_ARGUMENT);
 }
 
-fn count(input: impl BufRead) -> HashMap<String, usize> {
-    let word_regex = Regex::new(r"\w+").unwrap();
-    let mut freqs = HashMap::new();
-
+fn calculate_frequency(input: impl BufRead) -> HashMap<String, usize> {
+    let mut m = HashMap::new();
     for line in input.lines() {
-        let line = line.unwrap();
-        for m in word_regex.find_iter(&line) {
-            let word = m.as_str().to_string();
-            *freqs.entry(word).or_insert(0) += 1;
+        for mat in Regex::new(r"\w+").unwrap().find_iter(&line.unwrap()) {
+            // or_insert(0) では、マッチした word に該当する HashMap の Entry の Value に初期値 0 を設定しつつミュータブルな参照を取得している。
+            // そしてそのミュータブルな参照をさらに * で Deref して実体の usize のデータ取得しインクリメントしている。
+            *m.entry(mat.as_str().to_string()).or_insert(0) += 1;
         }
     }
-
-    freqs
+    m
 }
