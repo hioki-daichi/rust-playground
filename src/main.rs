e231d642780ca1d19fd1f60743c21ccd37b7965b
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-11 14:35:13 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+mod server {
+    pub fn echo() {
+        println!("Server")
+    }
+}
+
+mod client {
+    pub fn echo() {
+        println!("Client")
+    }
+}
+
 fn main() {
-    let mut one = 1;
-    // move キーワードを使うことで、変数を貸すのではなくコピーさせる。
-    // 整数は Copy トレイトを実装しているため所有権を移転するのではなくコピーされる。
-    let plus_one = move |x| x + one;
-    one += 1;
-    println!("10 + 1 = {}", plus_one(10)); // 10 + 1 = 11
-    println!("one = {}", one); // 2
+    server::echo();
+    client::echo();
 }
