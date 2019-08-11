a0dbde4cc1c66f5e520b13e21c2854618f08a442
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-11 22:20:24 +0900

    Update

--- wordcount/src/lib.rs
+++ wordcount/src/lib.rs
@@ -63,10 +63,6 @@ fn increment(m: &mut HashMap<String, usize>, key: impl ToString) {
 
 #[test]
 fn result_test() -> std::io::Result<()> {
-    use std::fs::{read_to_string, remove_file, write};
-    write("test.txt", "message")?;
-    let message = read_to_string("test.txt")?;
-    remove_file("test.txt")?;
-    assert_eq!(message, "message");
-    Ok(())
+    use std::fs::remove_file;
+    remove_file("no_such_file")
 }
