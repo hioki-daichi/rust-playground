9712e7a00389dd41be6787aa91fd71abf7dfb914
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-11 13:55:01 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,7 @@
 fn main() {
-    let mut counter = Some(0);
+    let v1 = vec!["foo", "bar", "baz"];
 
-    // while let 式。パターンマッチに成功した場合にループ本体が実行される。
-    while let Some(i) = counter {
-        if i == 10 {
-            counter = None;
-        } else {
-            println!("{}", i);
-            counter = Some(i + 1);
-        }
+    for s in v1.iter() {
+        println!("{}", s);
     }
-    // => 0
-    // => 1
-    // => 2
-    // => 3
-    // => 4
-    // => 5
-    // => 6
-    // => 7
-    // => 8
-    // => 9
 }
