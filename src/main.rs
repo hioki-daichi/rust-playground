ccf147fd42ee35d3db97fe423cbc51b625b971c1
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-11 13:38:32 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 fn main() {
-    let a = 12;
+    let o1 = Some("Apple");
 
-    let even_or_odd = if a % 2 == 0 { "an even" } else { "an odd" };
+    // 複合的な型 (e.g. 列挙型, 構造体, タプル, 参照)
+    // の値からその中の値を取り出すことを「分配束縛」と呼ぶ。
+    let s1 = match o1 {
+        // o1 がバリアント Option::Some の場合に、
+        // 変数 s2 をバリアントに関連付けられている値 ("Apple") に束縛している。
+        Some(s2) => String::from("Hi, ") + s2,
+        None => String::from("Nothing"),
+    };
 
-    println!("{} is {} number", a, even_or_odd); // 12 is an even number
+    println!("{}", s1); // Hi, Apple
 }
