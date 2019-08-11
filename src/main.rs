5d7b1256ea0a93a64c251e30f54d2f2d1df6dc3d
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-11 14:45:05 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
-mod server {
-    pub fn echo() {
-        println!("Server")
-    }
-}
+#[allow(unused_imports)]
+// rand は外部のクレートなので絶対指定。
+// rand クレートの prelude モジュールの中のすべてのアイテムを使っている。
+use rand::prelude::*;
 
-mod client {
-    pub fn echo() {
-        println!("Client")
+mod network {
+    pub fn ping() {
+        println!("Ping");
     }
 }
 
 fn main() {
-    server::echo();
-    client::echo();
+    // ルートモジュールからの相対指定
+    network::ping(); // Ping
+
+    // 明示的にクレートのルートモジュールを表す crate を書いてもよい
+    crate::network::ping(); // Ping
+
+    // self は現在地のモジュールを指すが、現在地はルートモジュールなので次のようにも書ける
+    self::network::ping(); // Ping
 }
