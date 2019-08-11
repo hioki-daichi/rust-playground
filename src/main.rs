cd475caff7eed2769a50818562f1a90341fdd8c7
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-11 14:04:33 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 fn main() {
-    let one = 1;
-    let plus_one = |x| x + one;
+    let mut one = 1;
+    // move キーワードを使うことで、変数を貸すのではなくコピーさせる。
+    // 整数は Copy トレイトを実装しているため所有権を移転するのではなくコピーされる。
+    let plus_one = move |x| x + one;
+    one += 1;
     println!("10 + 1 = {}", plus_one(10)); // 10 + 1 = 11
+    println!("one = {}", one); // 2
 }
