7099063ed6d0ceecb01164063ad444c4dd969c8e
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-11 13:30:26 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-#[allow(overflowing_literals)]
 fn main() {
-    let u: u8 = 0b11111111;
-    let i: i8 = 0b11111111;
+    let a = 12;
 
-    println!("{:08b}", u >> 2); // 00111111 (論理右シフト演算)
-    println!("{:08b}", i >> 2); // 11111111 (算術右シフト演算)
+    let even_or_odd = if a % 2 == 0 { "an even" } else { "an odd" };
+
+    println!("{} is {} number", a, even_or_odd); // 12 is an even number
 }
