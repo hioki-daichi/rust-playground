f21f56b63157610b96e6ab64236297b37b03e543
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-15 20:46:38 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ struct Parent(u32, Child, Child);
 #[derive(Debug)]
 struct Child(u32);
 
-fn f(p: &Parent) {
+fn f(p: &mut Parent) {
     println!("{:?}", p);
 }
 
 fn main() {
-    let p1 = Parent(1, Child(11), Child(12));
+    let mut p1 = Parent(1, Child(11), Child(12));
 
-    f(&p1); // Parent(1, Child(11), Child(12))
+    f(&mut p1); // Parent(1, Child(11), Child(12))
 
     println!("{:?}", p1); // Parent(1, Child(11), Child(12))
 }
