0d776fda3aefe4a19b14af4f5535ee52996498c6
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-15 23:17:08 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,67 @@
-use std::collections::HashMap;
+pub struct ToyVec<T> {
+    elements: Box<[T]>,
+    len: usize,
+}
 
-// 指定した k が存在しない場合: (k, "?") のように "?" を Value に持つ Entry を作成する。
-// 指定した k が存在する場合:   (k, "<元のValue>!") のように "!" を追記する。
-fn f(k: char, m: &mut HashMap<char, String>) {
-    // get_mut の第1引数は &mut self なので m の可変の借用をとっている
-    let o: Option<&mut String> = m.get_mut(&k);
+impl<T: Default> ToyVec<T> {
+    pub fn new() -> Self {
+        Self::with_capacity(0)
+    }
 
-    // o (可変の参照) が生存している間は m の可変の借用が有効。
-    // m に対する可変の借用が 2 つ同時に存在してはいけない。
+    pub fn with_capacity(capacity: usize) -> Self {
+        Self {
+            elements: Self::allocate_in_heap(capacity),
+            len: 0,
+        }
+    }
 
-    match o {
-        // Some(s) の腕では s を戻り値に束縛しているためこの腕の中まで m の可変の借用が有効になる。
-        Some(s) => s.push_str("!"),
-        // None の腕では戻り値に何も束縛しておらず get_mut による m の可変の借用は終了している
-        // ため insert は問題なく m の可変の借用をとれる
-        None => {
-            // insert の第1引数は &mut self なので m の可変の借用をとっている
-            m.insert(k, String::from("?"));
+    fn allocate_in_heap(size: usize) -> Box<[T]> {
+        std::iter::repeat_with(Default::default)
+            .take(size)
+            .collect::<Vec<_>>()
+            .into_boxed_slice()
+    }
+
+    pub fn len(&self) -> usize {
+        self.len
+    }
+
+    pub fn capacity(&self) -> usize {
+        self.elements.len()
+    }
+
+    pub fn push(&mut self, element: T) {
+        if self.len == self.capacity() {
+            self.grow();
+        }
+        self.elements[self.len] = element;
+        self.len += 1;
+    }
+
+    pub fn get(&self, index: usize) -> Option<&T> {
+        if index < self.len {
+            Some(&self.elements[index])
+        } else {
+            None
+        }
+    }
+
+    fn grow(&mut self) {
+        if self.capacity() == 0 {
+            self.elements = Self::allocate_in_heap(1);
+        } else {
+            let new_elements = Self::allocate_in_heap(self.capacity() * 2);
+            let old_elements = std::mem::replace(&mut self.elements, new_elements);
+            for (i, elem) in old_elements.into_vec().into_iter().enumerate() {
+                self.elements[i] = elem;
+            }
         }
     }
 }
 
 fn main() {
-    let mut m = HashMap::new();
-
-    f('a', &mut m);
-    println!("{:?}", m); // {'a': "?"}
-
-    f('a', &mut m);
-    println!("{:?}", m); // {'a': "?!"}
-
-    f('a', &mut m);
-    println!("{:?}", m); // {'a': "?!!"}
+    let mut v = ToyVec::new();
+    v.push("Java Finch".to_string());
+    let e = v.get(0);
+    assert_eq!(e, Some(&"Java Finch".to_string()))
 }
