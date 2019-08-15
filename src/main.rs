2012159426fe4d08249f4406e26491d172398f31
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-15 20:11:57 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
-#[derive(Debug)]
+// Copy, Clone トレイトを実装している
+#[derive(Copy, Clone, Debug)]
 struct Parent(u32, Child, Child);
 
-#[derive(Debug)]
+// Copy, Clone トレイトを実装している
+#[derive(Copy, Clone, Debug)]
 struct Child(u32);
 
 fn main() {
-    let p = Parent(1, Child(11), Child(12)); // borrow of moved value: `p`    move occurs because `p` has type `Parent`, which does not implement the `Copy` trait
-
-    // move が付いている
-    let f = move |x: u32| x + p.0; // borrow of moved value: `p`    variable moved due to use in closure
-
-    // コンパイルエラー
-    println!("{} {:?}", f(1), p); // borrow of moved value: `p`    value borrowed here after move
+    let p1 = Parent(1, Child(11), Child(12));
+    let p2 = p1;
+    println!("{:?} {:?}", p1, p2);
 }
