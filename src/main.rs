99906a2f167a66c82ba03bbf85e9a519c61305de
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-15 19:53:21 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,12 @@ struct Parent(u32, Child, Child);
 #[derive(Debug)]
 struct Child(u32);
 
-fn f() -> Parent {
-    let p = Parent(1, Child(11), Child(12));
-    p
-}
-
 fn main() {
-    let p1 = Some(Parent(1, Child(11), Child(12)));
-    match p1 {
-        Some(p2) => println!("{:?}", p2), // borrow of moved value: `p1`    value moved here    note: move occurs because value has type `Parent`, which does not implement the `Copy` trait
-        None => println!("none"),
-    }
-    println!("{:?}", p1); // borrow of moved value: `p1`    value borrowed here after partial move    note: move occurs because value has type `Parent`, which does not implement the `Copy` trait
+    let p = Parent(1, Child(11), Child(12)); // borrow of moved value: `p`    move occurs because `p` has type `Parent`, which does not implement the `Copy` trait
+
+    // move が付いている
+    let f = move |x: u32| x + p.0; // borrow of moved value: `p`    variable moved due to use in closure
+
+    // コンパイルエラー
+    println!("{} {:?}", f(1), p); // borrow of moved value: `p`    value borrowed here after move
 }
