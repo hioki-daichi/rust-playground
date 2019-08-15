b6af0d672188ea90c86db5473fd8d6202fb71ff4
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-15 20:37:27 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ struct Parent(u32, Child, Child);
 #[derive(Debug)]
 struct Child(u32);
 
-fn f(p: Parent) {
+fn f(p: &Parent) {
     println!("{:?}", p);
 }
 
 fn main() {
     let p1 = Parent(1, Child(11), Child(12));
 
-    f(p1);
+    f(&p1); // Parent(1, Child(11), Child(12))
 
-    println!("{:?}", p1); // borrow of moved value: `p1`    value borrowed here after move
+    println!("{:?}", p1); // Parent(1, Child(11), Child(12))
 }
