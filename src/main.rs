8649ebcadd974dfde4cda45dc60b60a6d56de12b
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-16 22:24:01 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,22 @@
-fn main() {}
+struct ToyVec<T> {
+    elements: Box<[T]>,
+}
+
+impl<T: Default> ToyVec<T> {
+    pub fn new(n: usize) -> Self {
+        Self {
+            elements: std::iter::repeat_with(Default::default)
+                .take(n)
+                .collect::<Vec<_>>()
+                .into_boxed_slice(),
+        }
+    }
+}
+
+fn main() {
+    let v: ToyVec<i32> = ToyVec::new(2);
+
+    println!("{:?}", v.elements.get(0)); // Some(0)
+    println!("{:?}", v.elements.get(1)); // Some(0)
+    println!("{:?}", v.elements.get(2)); // None
+}
