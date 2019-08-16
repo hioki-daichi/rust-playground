c572f67b7c128e351212304a4544c8ff40bb053e
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-16 22:15:53 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,59 @@
-fn f(n: usize) -> Box<[i32]> {
-    let f: fn() -> i32 = Default::default;
-    println!("{}", f()); // 0
+struct ToyVec<T> {
+    elements: Box<[T]>,
+    len: usize,
+}
 
-    let a: std::iter::RepeatWith<fn() -> i32> = std::iter::repeat_with(f);
-    println!("{:?}", a); // RepeatWith { repeater: 0x10d3b2480 }
+impl<T: Default> ToyVec<T> {
+    pub fn new() -> Self {
+        Self::with_capacity(0)
+    }
 
-    let b: std::iter::Take<std::iter::RepeatWith<fn() -> i32>> = a.take(n);
-    println!("{:?}", b); // Take { iter: RepeatWith { repeater: 0x10d3b2480 }, n: 2 }
+    fn with_capacity(capacity: usize) -> Self {
+        Self {
+            elements: Self::allocate_in_heap(capacity),
+            len: capacity,
+        }
+    }
 
-    let c: std::vec::Vec<i32> = b.collect::<Vec<_>>();
+    fn allocate_in_heap(size: usize) -> Box<[T]> {
+        std::iter::repeat_with(Default::default)
+            .take(size)
+            .collect::<Vec<_>>()
+            .into_boxed_slice()
+    }
 
-    c.into_boxed_slice()
+    fn capacity(&self) -> usize {
+        self.len
+    }
+
+    pub fn push(&mut self, e: T) {
+        if self.elements.len() == self.capacity() {
+            self.grow();
+        }
+        self.elements[self.len] = e;
+        self.len += 1;
+    }
+
+    fn grow(&mut self) {
+        if self.capacity() == 0 {
+            self.elements = Self::allocate_in_heap(1);
+        } else {
+            let new_elements = Self::allocate_in_heap(self.capacity() * 2);
+            let old_elements = std::mem::replace(&mut self.elements, new_elements);
+            for (i, elem) in old_elements.into_vec().into_iter().enumerate() {
+                self.elements[i] = elem;
+            }
+        }
+    }
+
+    pub fn pop(&self) -> Option<&T> {
+        self.elements.get(self.len - 1)
+    }
 }
 
 fn main() {
-    println!("{:?}", f(2)); // [0, 0]
+    let mut v = ToyVec::new();
+    v.push(1);
+    let x = v.pop();
+    println!("{:?}", x);
 }
