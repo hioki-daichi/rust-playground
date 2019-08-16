c8a57028981dd44f03750778dad4974cbd62b4b5
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-16 21:38:36 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
-fn main() {
+fn f(n: usize) -> Box<[i32]> {
     let f: fn() -> i32 = Default::default;
     println!("{}", f()); // 0
 
     let a: std::iter::RepeatWith<fn() -> i32> = std::iter::repeat_with(f);
     println!("{:?}", a); // RepeatWith { repeater: 0x10d3b2480 }
 
-    let b: std::iter::Take<std::iter::RepeatWith<fn() -> i32>> = a.take(2);
+    let b: std::iter::Take<std::iter::RepeatWith<fn() -> i32>> = a.take(n);
     println!("{:?}", b); // Take { iter: RepeatWith { repeater: 0x10d3b2480 }, n: 2 }
 
     let c: std::vec::Vec<i32> = b.collect::<Vec<_>>();
-    println!("{:?}", c); // [0, 0]
 
-    let d: std::boxed::Box<[i32]> = c.into_boxed_slice();
-    println!("{:?}", d); // [0, 0]
+    c.into_boxed_slice()
+}
+
+fn main() {
+    println!("{:?}", f(2)); // [0, 0]
 }
