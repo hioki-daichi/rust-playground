7089419ae224bb0fc11d5f4d9baa4e5e9ae4d49b
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-16 21:22:09 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,16 @@
-pub struct ToyVec<T> {
-    elements: Box<[T]>,
-    len: usize,
-}
-
-impl<T: Default> ToyVec<T> {
-    pub fn new() -> Self {
-        Self::with_capacity(0)
-    }
-
-    pub fn with_capacity(capacity: usize) -> Self {
-        Self {
-            elements: Self::allocate_in_heap(capacity),
-            len: 0,
-        }
-    }
-
-    fn allocate_in_heap(size: usize) -> Box<[T]> {
-        std::iter::repeat_with(Default::default)
-            .take(size)
-            .collect::<Vec<_>>()
-            .into_boxed_slice()
-    }
-
-    pub fn len(&self) -> usize {
-        self.len
-    }
-
-    pub fn capacity(&self) -> usize {
-        self.elements.len()
-    }
-
-    pub fn push(&mut self, element: T) {
-        if self.len == self.capacity() {
-            self.grow();
-        }
-        self.elements[self.len] = element;
-        self.len += 1;
-    }
-
-    pub fn get(&self, index: usize) -> Option<&T> {
-        if index < self.len {
-            Some(&self.elements[index])
-        } else {
-            None
-        }
-    }
-
-    fn grow(&mut self) {
-        if self.capacity() == 0 {
-            self.elements = Self::allocate_in_heap(1);
-        } else {
-            let new_elements = Self::allocate_in_heap(self.capacity() * 2);
-            let old_elements = std::mem::replace(&mut self.elements, new_elements);
-            for (i, elem) in old_elements.into_vec().into_iter().enumerate() {
-                self.elements[i] = elem;
-            }
-        }
-    }
-}
-
 fn main() {
-    let mut v = ToyVec::new();
-    v.push("Java Finch".to_string());
-    let e = v.get(0);
-    assert_eq!(e, Some(&"Java Finch".to_string()))
+    let f: fn() -> i32 = Default::default;
+    println!("{}", f()); // 0
+
+    let a: std::iter::RepeatWith<fn() -> i32> = std::iter::repeat_with(f);
+    println!("{:?}", a); // RepeatWith { repeater: 0x10d3b2480 }
+
+    let b: std::iter::Take<std::iter::RepeatWith<fn() -> i32>> = a.take(2);
+    println!("{:?}", b); // Take { iter: RepeatWith { repeater: 0x10d3b2480 }, n: 2 }
+
+    let c: std::vec::Vec<i32> = b.collect::<Vec<_>>();
+    println!("{:?}", c); // [0, 0]
+
+    let d: std::boxed::Box<[i32]> = c.into_boxed_slice();
+    println!("{:?}", d); // [0, 0]
 }
