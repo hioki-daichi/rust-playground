5e857a8b3e2a69b5f4f6b42dcab51f025ffdfb28
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-16 22:52:50 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,25 @@ impl<T: Default> ToyVec<T> {
 
     pub fn with_capacity(n: usize) -> Self {
         Self {
-            elements: std::iter::repeat_with(Default::default)
-                .take(n)
-                .collect::<Vec<_>>()
-                .into_boxed_slice(),
+            elements: Self::allocate_in_heap(n),
         }
+    }
+
+    fn allocate_in_heap(n: usize) -> Box<[T]> {
+        std::iter::repeat_with(Default::default)
+            .take(n)
+            .collect::<Vec<_>>()
+            .into_boxed_slice()
+    }
+
+    pub fn push(&mut self, e: T) {
+        self.elements[self.elements.len()] = e;
     }
 }
 
 fn main() {
-    let v: ToyVec<i32> = ToyVec::new();
+    let mut v: ToyVec<i32> = ToyVec::new();
 
-    println!("{:?}", v.elements.get(0)); // Some(0)
-    println!("{:?}", v.elements.get(1)); // Some(0)
-    println!("{:?}", v.elements.get(2)); // None
+    // 実行時エラー
+    v.push(0); // thread 'main' panicked at 'index out of bounds: the len is 0 but the index is 0', src/main.rs:24:9
 }
