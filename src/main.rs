37c579dc2e16a88f5fac29dfff797cdd4beb19e1
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-18 12:13:44 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,106 +1,20 @@
-#[derive(Debug)]
-struct ToyVec<T> {
-    elements: Box<[T]>,
-    len: usize, // ベクタの長さ (現在の要素数)
-}
-
-impl<T: Default> ToyVec<T> {
-    fn new() -> Self {
-        Self::with_capacity(0)
-    }
-
-    // 第1引数が &mut self なので ToyVec 構造体の内容を変更することがわかる
-    // 第2引数が T 型なので所有権がこのメソッドへムーブすることがわかる
-    fn push(&mut self, element: T) {
-        // 要素を追加するスペースがないなら
-        if self.len == self.capacity() {
-            // もっと大きい elements を確保して既存の要素を引っ越す
-            self.grow();
-        }
-
-        // self.grow() で十分なキャパシティが確保できたら
-        // element を Box<[T]> へムーブする
-        self.elements[self.len] = element; // 所有権がムーブしている
-        self.len += 1;
-    }
-
-    // 第1引数が &self なので ToyVec 構造体の内容は変更されないことがわかる
-    // 第2引数は usize なので値がコピーされる
-    // Option<&T> を返すため self が所有する値の不変の参照を返すことがわかる
-    fn get(&self, index: usize) -> Option<&T> {
-        if index < self.len {
-            Some(&self.elements[index])
-        } else {
-            None
-        }
-    }
-
-    // fn get_or<'a>(&'a mut self, index: usize, default: &'a T) -> &'a T {
-    //     self.get(index).unwrap_or(default)
-    // }
-
-    // fn pop(&mut self) -> Option<T> {
-    //     if self.len == 0 {
-    //         None
-    //     } else {
-    //         self.len -= 1;
-    //         let elem = std::mem::replace(&mut self.elements[self.len], Default::default());
-    //         Some(elem)
-    //     }
-    // }
-
-    fn with_capacity(n: usize) -> Self {
-        Self {
-            elements: Self::allocate_in_heap(n),
-            len: n,
-        }
-    }
-
-    fn allocate_in_heap(n: usize) -> Box<[T]> {
-        std::iter::repeat_with(Default::default)
-            .take(n)
-            .collect::<Vec<_>>()
-            .into_boxed_slice()
-    }
-
-    fn grow(&mut self) {
-        if self.capacity() == 0 {
-            self.elements = Self::allocate_in_heap(1);
-        } else {
-            // 現在の 2 倍の長さの Box<[T]> を作成し、
-            let new_elements = Self::allocate_in_heap(self.capacity() * 2);
-            // 既存の全要素を
-            let old_elements = std::mem::replace(&mut self.elements, new_elements);
-            for (i, element) in old_elements.into_vec().into_iter().enumerate() {
-                // self.elements にセットする。
-                self.elements[i] = element;
-            }
-        }
-    }
-
-    fn capacity(&self) -> usize {
-        self.elements.len()
-    }
-}
-
 fn main() {
-    let mut v = ToyVec::new();
+    // static 変数。'static スコープを持つ。
+    static I0: i32 = 42;
 
-    v.push("Java Finch".to_string()); // 桜文鳥
-    v.push("Budgerigar".to_string()); // セキセイインコ
+    // 'static スコープを持つ str への可変の参照
+    let mut s0: &'static str;
 
-    // この時点では、スタック領域に v と e のためのスペースが用意されている。
-    // v は ToyVec の構造体が格納されていて、e はまだ初期化されていない。
-    //
-    // スタック領域では ToyVec<String> の構造体が 3 行占めていて、
-    //
-    // - 上 2 行が Box<[String]> 型の elements フィールド
-    //     - スライスを実現するデータ構造
-    //     - 以下からなるファットポインタ (ファットポインタとは、データへのポインタといくつかの追加情報を含む構造体のこと)
-    //         - スライスの先頭要素を指すポインタ
-    //         - スライスの要素数
-    // - 下 1 行が len フィールド
-    //     - ベクタの要素数で、この時点では 2 になっている。
-    let e = v.get(1);
-    assert_eq!(e, Some(&"Budgerigar".to_string()));
+    // 文字列リテラルへの参照。データは静的領域にある。
+    let s1: &str = "42";
+
+    // String 型。データはヒープ領域にある。
+    let s2 = 42.to_string();
+
+    // 文字列リテラルへの参照は 'static ライフタイムを持つため束縛可能。
+    s0 = s1;
+
+    // コンパイルエラー
+    // String 型から &'static str は作れない。
+    // s0 = &s2; // `s2` does not live long enough    borrowed value does not live long enough
 }
