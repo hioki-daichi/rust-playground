a9ebbbeea06a5e5aa94799bb2e0ce8759010845e
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-18 17:21:06 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,15 @@ mod toy_vec;
 use toy_vec::*;
 
 fn main() {
-    let mut v = ToyVec::new();
+    let mut v: ToyVec<usize> = ToyVec::new();
+    v.push(100);
 
-    v.push("Java Finch".to_string()); // 桜文鳥
-    v.push("Budgerigar".to_string()); // セキセイインコ
+    // 不変の借用が取得されている
+    let e = v.get(0);
 
-    // この時点では、スタック領域に v と e のためのスペースが用意されている。
-    // v は ToyVec の構造体が格納されていて、e はまだ初期化されていない。
-    //
-    // スタック領域では ToyVec<String> の構造体が 3 行占めていて、
-    //
-    // - 上 2 行が Box<[String]> 型の elements フィールド
-    //     - スライスを実現するデータ構造
-    //     - 以下からなるファットポインタ (ファットポインタとは、データへのポインタといくつかの追加情報を含む構造体のこと)
-    //         - スライスの先頭要素を指すポインタ
-    //         - スライスの要素数
-    // - 下 1 行が len フィールド
-    //     - ベクタの要素数で、この時点では 2 になっている。
-    let e = v.get(1);
-    assert_eq!(e, Some(&"Budgerigar".to_string()));
+    // コンパイルエラー
+    // ベクタを変更しようとしているが、後に不変の借用 e にアクセスしているため借用規則に反している
+    v.push(200);
 
-    let e2 = v.pop();
-    assert_eq!(e2, Some("Budgerigar".to_string()));
-
-    let foo = "foo".to_string();
-    let e3 = v.get_or(2, &foo);
-    assert_eq!(e3, &foo);
+    assert_eq!(e, Some(&100));
 }
