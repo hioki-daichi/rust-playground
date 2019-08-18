3b1f55f3ec2e1eb7cd79df03d41f6f215548708d
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-18 12:20:02 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,15 @@
+// ジェネリクスにライフタイム境界を付ける場合
+// 'static ライフタイムを持つ任意の型を引数にとる
+fn take_static<T: 'static>(_x: T) {}
+
 fn main() {
-    // static 変数。'static スコープを持つ。
-    static I0: i32 = 42;
-
-    // 'static スコープを持つ str への可変の参照
-    let mut s0: &'static str;
-
-    // 文字列リテラルへの参照。データは静的領域にある。
-    let s1: &str = "42";
-
-    // String 型。データはヒープ領域にある。
+    let s1 = "42";
     let s2 = 42.to_string();
 
-    // 文字列リテラルへの参照は 'static ライフタイムを持つため束縛可能。
-    s0 = s1;
+    take_static(s1); // OK
 
     // コンパイルエラー
-    // String 型から &'static str は作れない。
-    // s0 = &s2; // `s2` does not live long enough    borrowed value does not live long enough
+    take_static(&s2); // `s2` does not live long enough    borrowed value does not live long enough
+
+    take_static(s2); // OK
 }
