ec83ec54a1100bdb36c9dece8970438404aa5e90
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-19 23:25:20 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,31 @@
-use std::cell::RefCell;
 use std::collections::HashSet;
+use std::error::Error;
+use std::sync::{Arc, RwLock};
 
-fn main() {
-    // 標準ライブラリの thread_local! マクロで TLS に変数 SS を作成している
-    thread_local!(
-        static SS: RefCell<HashSet<&'static str>> = {
-            // >> let ss: HashSet<&str> = ["foo", "bar"].iter().cloned().collect();
-            // >> ss
-            // {"bar", "foo"}
-            let ss = ["foo", "bar"].iter().cloned().collect();
-            RefCell::new(ss)
-        }
-    );
+fn main() -> Result<(), Box<dyn Error>> {
+    let a: HashSet<_> = ["foo", "bar"].iter().cloned().collect();
+    let a = Arc::new(RwLock::new(a));
 
-    // TLS に置いた値にアクセスするには with を使う。
-    SS.with(|ss| {
-        // ss の型は &RefCell<HashSet<&'static str>>
+    fn stringify(a: impl ToString) -> String {
+        a.to_string()
+    }
 
-        assert!(ss.borrow().contains("foo"));
-        assert!(ss.borrow().contains("bar"));
+    {
+        let b = a.read().map_err(stringify)?; // read ロックを取得している
+        assert!(b.contains("foo"));
+        assert!(b.contains("bar"));
+    } // b がスコープをはずれロックが解除される
 
-        // "baz" を追加する。
-        assert!(!ss.borrow().contains("baz"));
-        ss.borrow_mut().insert("baz");
-        assert!(ss.borrow().contains("baz"));
-    });
+    a.write().map_err(stringify)?.insert("baz");
 
-    // 別スレッドを起動する。
-    std::thread::spawn(|| {
-        SS.with(|ss| {
-            assert!(ss.borrow().contains("foo"));
-            assert!(ss.borrow().contains("bar"));
+    let c = Arc::clone(&a);
 
-            // main スレッドで追加した "baz" はこちらのスレッドには存在しない。
-            assert!(!ss.borrow().contains("baz"));
+    std::thread::spawn(move || c.write().map(|mut d| d.insert("qux")).map_err(stringify))
+        .join()
+        .expect("Thread error")?;
 
-            // "qux" を追加する。
-            assert!(!ss.borrow().contains("qux"));
-            ss.borrow_mut().insert("qux");
-            assert!(ss.borrow().contains("qux"));
-        });
-    })
-    .join()
-    .expect("Thread error");
+    assert!(a.read().map_err(stringify)?.contains("baz"));
+    assert!(a.read().map_err(stringify)?.contains("qux"));
 
-    // 再び main スレッド
-    SS.with(|ss| {
-        // 以前 main スレッドで追加した "baz" がいる。
-        assert!(ss.borrow().contains("baz"));
-        // 別スレッドで追加した "qux" はいない。
-        assert!(!ss.borrow().contains("qux"));
-    })
+    Ok(())
 }
