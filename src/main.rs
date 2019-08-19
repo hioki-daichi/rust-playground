9d5e41ecb611e7692a4d2cef0b0a6698761a9096
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-19 22:35:53 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,35 @@
+use std::cell::RefCell;
+
 struct A {
-    s: String,
+    s: RefCell<String>,
 }
 
 fn main() {
     let a = A {
-        s: "daichi".to_string(),
+        s: RefCell::new("Daichi".to_string()),
     };
 
     let ra = &a;
 
-    // コンパイラエラー
-    // 不変の参照経由でフィールドを変更しようとしている
-    ra.s.push('a'); // cannot borrow `ra.s` as mutable, as it is behind a `&` reference    `ra` is a `&` reference, so the data it refers to cannot be borrowed as mutable
+    // 可変の参照を取っている
+    ra.s.borrow_mut().push_str(" Hioki");
+
+    assert_eq!(*ra.s.borrow_mut(), "Daichi Hioki".to_string());
+
+    {
+        // 不変の参照を取っている
+        let ras = a.s.borrow();
+
+        assert_eq!(*ras, "Daichi Hioki".to_string());
+
+        // さらに a.s.borrow_mut(); で可変の参照を取ろうとすると panic する。
+        // thread 'main' panicked at 'already borrowed: BorrowMutError', src/libcore/result.rs:1084:5
+        //
+        // RefCell は実行時に貸し出しの数をカウントして借用規則に従っているかをチェックしてくれる。
+
+        // try_borrow_mut() なら借用の成否が Result で返る
+        assert!(a.s.try_borrow_mut().is_err())
+    } // ras はここでスコープを抜ける
+
+    assert!(a.s.try_borrow_mut().is_ok())
 }
