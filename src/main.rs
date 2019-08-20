4acce2de6599a4533328e7dc52c3e83e197ac13b
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-20 20:48:19 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,48 @@ use std::collections::HashSet;
 use std::error::Error;
 use std::sync::{Arc, RwLock};
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let a: HashSet<_> = ["foo", "bar"].iter().cloned().collect();
-    let a = Arc::new(RwLock::new(a));
+// read() や write() で発生する PoisonError を String に変換する便利な関数
+fn stringify(a: impl ToString) -> String {
+    a.to_string()
+}
 
-    fn stringify(a: impl ToString) -> String {
-        a.to_string()
-    }
+fn main() -> Result<(), Box<dyn Error>> {
+    let hs: HashSet<&'static str> = ["foo", "bar"].iter().cloned().collect();
+
+    // RwLock で包むことで HashSet を可変にして、
+    // Arc で包むことでスレッド間で共有できるようにしている
+    let hs: Arc<RwLock<HashSet<&'static str>>> = Arc::new(RwLock::new(hs));
 
     {
-        let b = a.read().map_err(stringify)?; // read ロックを取得している
-        assert!(b.contains("foo"));
-        assert!(b.contains("bar"));
-    } // b がスコープをはずれロックが解除される
+        // read ロックを取得している。
+        //
+        // Arc は内包するデータ型 (ここでは RwLock) への Deref を実装しているため、
+        // メソッドレシーバの型強制により RwLock のメソッドを直に呼べる。
+        //
+        // ロックが取得できない時は取得できるまで現在のスレッドがブロックされる。
+        //
+        // ロックが取得できると Ok(ガード) を返すため ? 演算子でアンラップしている。
+        //
+        let hs2 = hs.read().map_err(stringify)?;
 
-    a.write().map_err(stringify)?.insert("baz");
+        assert!(hs2.contains("foo"));
+        assert!(hs2.contains("bar"));
+    } // hs2 がスコープをはずれロックが解除される
 
-    let c = Arc::clone(&a);
+    // write ロックを取得している。
+    // Ok(ガード) を ? 演算子でアンラップした後、メソッドレシーバの型強制によって
+    // ガードから HashSet<&'static str> の insert() を呼んでいる。
+    hs.write().map_err(stringify)?.insert("baz");
 
-    std::thread::spawn(move || c.write().map(|mut d| d.insert("qux")).map_err(stringify))
-        .join()
+    // 複製した Arc ポインタを spawn で起動した別スレッドに渡すことで HashSet を共有している
+    let hs3: Arc<RwLock<HashSet<&'static str>>> = Arc::clone(&hs);
+    std::thread::spawn(move || hs3.write().map(|mut d| d.insert("qux")).map_err(stringify))
+        .join() // join() はスレッドの終了を待つ
         .expect("Thread error")?;
 
-    assert!(a.read().map_err(stringify)?.contains("baz"));
-    assert!(a.read().map_err(stringify)?.contains("qux"));
+    // このスレッドで別スレッドで追加した要素が見える。
+    assert!(hs.read().map_err(stringify)?.contains("baz"));
+    assert!(hs.read().map_err(stringify)?.contains("qux"));
 
     Ok(())
 }
