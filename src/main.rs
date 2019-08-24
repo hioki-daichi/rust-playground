7008b3d949b24de08892cde618a0500130f31118
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-24 21:07:31 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-use std::borrow::Borrow;
+// struct Foo<T>(T);
+// struct Bar<T: ?Sized>(T);
+//
+// // the size for values of type `[i32]` cannot be known at compilation time    doesn't have a size known at compile-time    help: the trait `std::marker::Sized` is not implemented for `[i32]`  note: to learn more, visit <https://doc.rust-lang.org/book/ch19-04-advanced-types.html#dynamically-sized-types-and-the-sized-trait>
+// // struct FooUse(Foo<[i32]>);
+// struct BarUse(Bar<[i32]>);
 
-fn f<T: Borrow<str> + ?Sized>(a: &T) {
-    let s: &str = a.borrow();
-    assert_eq!("Hello", s);
-}
+trait Foo {}
+trait Bar: Sized {}
 
-fn main() {
-    f("Hello");
-    f(&"Hello".to_string());
-}
+struct Impl;
+impl Foo for Impl {}
+impl Bar for Impl {}
+
+let x: &dyn Foo = &Impl;
+let x: &dyn Bar = &Impl;
