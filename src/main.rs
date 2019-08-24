b1ece4b2a366e9c2fcf9826b03597263a2116edf
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-24 16:48:49 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,15 @@
-fn main() {}
+fn f<T: std::borrow::Borrow<str>>(s: T) {
+    assert_eq!("Hello", s.borrow());
+}
+
+fn g<T: AsRef<str>>(s: T) {
+    assert_eq!("Hello", s.as_ref());
+}
+
+fn main() {
+    f("Hello");
+    f("Hello".to_string());
+
+    g("Hello");
+    g("Hello".to_string());
+}
