919083578e5f9126d8657d4a4648f038addc27ae
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-29 21:21:43 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct CartesianCoord {
     x: f64,
     y: f64,
@@ -52,7 +53,43 @@ impl Coordinates for (f64, f64) {
     }
 }
 
-fn print_point<P>(point: P) {
-    let p = point.to_cartesian();
-    println!("({} {})", p.x, p.y);
+struct Matrix([[f64; 2]; 2]);
+
+trait LinearTransform: Coordinates {
+    fn transform(self, matrix: &Matrix) -> Self;
+
+    fn rotate(self, theta: f64) -> Self
+    where
+        Self: Sized,
+    {
+        self.transform(&Matrix([
+            [theta.cos(), -theta.sin()],
+            [theta.sin(), theta.cos()],
+        ]))
+    }
+}
+
+impl LinearTransform for CartesianCoord {
+    fn transform(mut self, matrix: &Matrix) -> Self {
+        let x = self.x;
+        let y = self.y;
+        let m = matrix.0;
+
+        self.x = m[0][0] * x + m[0][1] * y;
+        self.y = m[1][0] * x + m[1][1] * y;
+        self
+    }
+}
+
+fn print_point<P: Coordinates>(point: P) {
+    let cart = point.to_cartesian();
+    println!("({} {})", cart.x, cart.y);
+}
+
+fn main() {
+    let p = (1.0, 0.0).to_cartesian();
+
+    // print_point(p); // (1 0)
+
+    print_point(p.rotate(std::f64::consts::PI)); // (-1 0.00000000000000012246467991473532)
 }
