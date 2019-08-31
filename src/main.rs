89aefc3a3f5dd3ba8ae5be39011c1144eeeacfa3
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-31 23:23:42 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,12 @@ impl Server for EchoServer {
     }
 }
 
+// 関連型が特定の型を持っていることを指定したい場合は `トレイト名<関連型名 = 型>` のように指定できる。
+// `Server<Request = String>` のように書いた場合、Request に String を持つ Server の実装しか受け付けない。
+fn handle<S: Server<Request = String>>(server: S, req: &str) -> S::Response {
+    server.handle(req.to_string())
+}
+
 fn main() {
-    let es = EchoServer {};
-    let res = es.handle("REQUEST".to_string());
-    println!("{:?}", res); // "REQUEST"
+    println!("{:?}", handle(EchoServer {}, "REQUEST")); // "REQUEST"
 }
