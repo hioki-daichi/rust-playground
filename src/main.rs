bee2bd7b2967b0f6969d53e1091efd678c585732
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-31 21:32:23 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,20 @@
-trait Overload {
-    fn call(&self) -> &'static str;
+trait Overload1<T> {
+    fn call(&self, t: T) -> &'static str;
 }
 
-impl Overload for i32 {
-    fn call(&self) -> &'static str {
-        "i32"
+impl Overload1<i32> for i32 {
+    fn call(&self, _: i32) -> &'static str {
+        "(i32, i32)"
     }
 }
 
-impl Overload for str {
-    fn call(&self) -> &'static str {
-        "str"
+impl Overload1<char> for i32 {
+    fn call(&self, _: char) -> &'static str {
+        "(i32, char)"
     }
 }
 
 fn main() {
-    assert_eq!(1i32.call(), "i32");
-    assert_eq!("str".call(), "str");
-
-    // Rust のメソッド呼び出しはただの糖衣構文なので以下のようにも書ける。
-    assert_eq!(Overload::call(&1i32), "i32");
-    assert_eq!(Overload::call("str"), "str");
+    assert_eq!(1i32.call(2i32), "(i32, i32)");
+    assert_eq!(1i32.call('c'), "(i32, char)");
 }
