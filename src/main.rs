4505e30330c4ac9e8d8cff2f04c183e09408bde4
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-31 21:12:38 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
-trait Init<T> {
-    fn init(t: T) -> Self;
+trait As<T> {
+    fn cast(self) -> T;
 }
 
-impl<T> Init<T> for Box<T> {
-    fn init(t: T) -> Self {
-        Box::new(t)
+impl As<u64> for u8 {
+    fn cast(self) -> u64 {
+        self as u64
+    }
+}
+
+impl As<u32> for u8 {
+    fn cast(self) -> u32 {
+        self as u32
     }
 }
 
 fn main() {
-    let data = Box::init("foo");
-    println!("{:?}", data); // "foo"
-
-    let data = Box::<f32>::init(0.1);
-    println!("{:?}", data); // 0.1
+    let one_u32: u32 = 1.cast();
+    let one_u32: u64 = 1.cast();
+    let one_u32: i8 = 1.cast(); // the trait bound `{integer}: As<i8>` is not satisfied    the trait `As<i8>` is not implemented for `{integer}`
 }
