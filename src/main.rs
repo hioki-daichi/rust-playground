2eb3da035651b139753f791e496145736da277cc
Daichi Hioki <hiokidaichi@gmail.com>
2019-09-01 01:03:02 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,8 @@
-use std::ops::Add;
-use std::ops::AddAssign;
-
-#[derive(Debug)]
-struct MyInt(i64);
-
-impl Add<MyInt> for MyInt {
-    type Output = Self;
-    fn add(self, rhs: MyInt) -> Self::Output {
-        MyInt(self.0 + rhs.0)
-    }
-}
-
-impl AddAssign for MyInt {
-    fn add_assign(&mut self, other: Self) {
-        self.0 += other.0;
-    }
+fn foo(s: impl Into<String>) {
+    println!("{:?}", s.into());
 }
 
 fn main() {
-    let one = MyInt(1);
-    let two = MyInt(2);
-    let mut i = one + two;
-    i += MyInt(3);
-    println!("{:?}", i); // MyInt(6)
+    foo("A"); // "A"
+    foo("B".to_string()); // "B"
 }
