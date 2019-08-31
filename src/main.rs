dc78414d79c3e885bf3ba57f7d4b5bea32a0c326
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-31 23:04:56 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
-struct Foo {}
+use std::str::FromStr;
 
-trait ATrait {
-    const AAA: u32;
+trait Server {
+    type Response;
+    type Request: FromStr; // FromStr がトレイト境界になる。
+
+    fn handle(&self, req: Self::Request) -> Self::Response;
 }
 
-impl ATrait for Foo {
-    const AAA: u32 = 2;
+struct EchoServer;
+
+impl Server for EchoServer {
+    type Response = String;
+    type Request = String; // トレイト境界
+    fn handle(&self, req: Self::Request) -> Self::Response {
+        req
+    }
 }
 
 fn main() {
-    println!("{}", Foo::AAA); // 2
+    let es = EchoServer {};
+    let res = es.handle("REQUEST".to_string());
+    println!("{:?}", res); // "REQUEST"
 }
