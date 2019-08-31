5ee85b254406ef42a1cf78da9d7a52aaea9c7f23
Daichi Hioki <hiokidaichi@gmail.com>
2019-08-31 22:10:25 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,6 @@
-trait Overload1<T> {
-    fn call(&self, t: T) -> &'static str;
-}
+use std::string::ToString;
 
-impl Overload1<i32> for i32 {
-    fn call(&self, _: i32) -> &'static str {
-        "(i32, i32)"
-    }
-}
-
-impl Overload1<char> for i32 {
-    fn call(&self, _: char) -> &'static str {
-        "(i32, char)"
-    }
-}
-
-fn main() {
-    assert_eq!(1i32.call(2i32), "(i32, i32)");
-    assert_eq!(1i32.call('c'), "(i32, char)");
+// 実際にはもう少し複雑な名前
+fn stringify_i32(t: i32) -> String {
+    <i32 as ToString>::to_string(t)
 }
