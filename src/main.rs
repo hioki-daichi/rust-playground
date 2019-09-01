dc52a3054349ab020ac956d9d5d54148423a9c5f
Daichi Hioki <hiokidaichi@gmail.com>
2019-09-01 14:44:14 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,4 @@
-use std::fs::File;
-use std::io;
-use std::os::unix::prelude::FileExt;
-
 fn main() -> io::Result<()> {
-    let mut buf = [0u8; 8];
-    let file = File::open("foo.txt")?; // "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    let num_bytes_read = file.read_at(&mut buf, 2)?;
-    println!("read {} bytes: {:?}", num_bytes_read, buf); // read 8 bytes: [67, 68, 69, 70, 71, 72, 73, 74]
-    Ok(())
+    let s = "foo";
+    let t = *s; // the size for values of type `str` cannot be known at compilation time
 }
