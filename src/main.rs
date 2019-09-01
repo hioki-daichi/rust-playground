bf507c2155dee1b3cd25cc7c0fdfd7c3bba64dfc
Daichi Hioki <hiokidaichi@gmail.com>
2019-09-01 13:47:59 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
 fn main() {
-    let a = [1, 2, 3, 4];
-    assert_eq!(a.iter().fold(0, |acc, x| acc + x), 10);
-    assert_eq!(a.iter().fold(1, |acc, x| acc * x), 24);
+    println!("{:?}", std::f64::NAN == std::f64::NAN); // false
 }
