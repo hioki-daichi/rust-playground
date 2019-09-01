47f8b354d8b6b473ac111d283b331d0c19566bf0
Daichi Hioki <hiokidaichi@gmail.com>
2019-09-01 12:19:16 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,8 @@ fn main() -> std::io::Result<()> {
     let mut buffer = File::create("foo.txt")?;
 
     while pos < data.len() {
-        println!("{:?}", buffer); // File { fd: 3, path: "/Users/daichi/.ghq/github.com/hioki-daichi/rust-playground/foo.txt", read: false, write: true }
-        let buf = &data[pos..];
-        println!("{:?}", buf); // [115, 111, 109, 101, 32, 98, 121, 116, 101, 115]
+        let buf = &data[pos..pos + 1];
         let bytes_written = buffer.write(buf)?;
-        println!("{:?}", bytes_written); // 10
         pos += bytes_written;
     }
     Ok(())
