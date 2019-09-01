ff45f1d4c9ffc067b9190dceade3caca79cbfe75
Daichi Hioki <hiokidaichi@gmail.com>
2019-09-01 13:44:47 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,5 @@
-use std::convert::From;
-use std::net::IpAddr;
-
 fn main() {
-    assert_eq!(usize::from(true), 1);
-    assert_eq!(usize::from(false), 0);
-
-    let xs: [u8; 4] = [192, 168, 0, 1];
-    let addr: IpAddr = IpAddr::from(xs);
-
-    println!("{:?}", addr); // V4(192.168.0.1)
+    let a = [1, 2, 3, 4];
+    assert_eq!(a.iter().fold(0, |acc, x| acc + x), 10);
+    assert_eq!(a.iter().fold(1, |acc, x| acc * x), 24);
 }
