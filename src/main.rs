cd3656b72d406cd5ac13e0eacea4f3d51e241ac3
Daichi Hioki <hiokidaichi@gmail.com>
2019-09-01 12:17:40 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
-fn foo(s: impl Into<String>) {
-    println!("{:?}", s.into());
-}
+use std::fs::File;
+use std::io::prelude::*;
 
-fn main() {
-    foo("A"); // "A"
-    foo("B".to_string()); // "B"
+fn main() -> std::io::Result<()> {
+    let data = b"some bytes";
+
+    let mut pos = 0;
+    let mut buffer = File::create("foo.txt")?;
+
+    while pos < data.len() {
+        println!("{:?}", buffer); // File { fd: 3, path: "/Users/daichi/.ghq/github.com/hioki-daichi/rust-playground/foo.txt", read: false, write: true }
+        let buf = &data[pos..];
+        println!("{:?}", buf); // [115, 111, 109, 101, 32, 98, 121, 116, 101, 115]
+        let bytes_written = buffer.write(buf)?;
+        println!("{:?}", bytes_written); // 10
+        pos += bytes_written;
+    }
+    Ok(())
 }
