a4dbb2db1f7eee558bb580f3d8d14fdc23bb2673
Daichi Hioki <hiokidaichi@gmail.com>
2019-09-01 15:09:29 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+use std::convert::Into;
+fn foo(min: impl Into<Option<usize>>, max: impl Into<Option<usize>>) -> [usize; 2] {
+    let a = min.into().unwrap_or(1);
+    let b = max.into().unwrap_or(100);
+    [a, b]
+}
+
 fn main() {
-    println!("{:?}", std::mem::size_of::<&String>()); // 8
-    println!("{:?}", std::mem::size_of::<&str>()); // 16
-    println!("{:?}", std::mem::size_of::<&i32>()); // 8
-    println!("{:?}", std::mem::size_of::<&[i32]>()); // 16
-    println!("{:?}", std::mem::size_of::<&[i32; 4]>()); // 8
-    println!("{:?}", std::mem::size_of::<&Vec<i32>>()); // 8
+    println!("{:?}", foo(30, 70)); // [30, 70]
+    println!("{:?}", foo(30, None)); // [30, 100]
+    println!("{:?}", foo(None, 70)); // [1, 70]
+    println!("{:?}", foo(None, None)); // [1, 100]
 }
