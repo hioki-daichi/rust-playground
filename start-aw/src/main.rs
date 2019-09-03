c97b8115f532213ca600cecbd6e1210ef9b3bf5e
Daichi Hioki <hiokidaichi@gmail.com>
2019-09-03 20:31:20 +0900

    Update

--- start-aw/src/main.rs
+++ start-aw/src/main.rs
@@ -1,15 +1,25 @@
-use actix_web::{server, App, HttpRequest, Responder};
+use actix_web::{server, App, Error, FromRequest, HttpRequest, Path, Responder};
+use serde_derive::*;
 
-fn hello(req: &HttpRequest) -> impl Responder {
-    let to = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", to)
+#[derive(Deserialize)]
+struct HelloPath {
+    name: String,
+}
+
+fn foo(req: &HttpRequest) -> Result<String, Error> {
+    let to = Path::<HelloPath>::extract(req)?;
+    Ok(format!("Hello {}!", &to.name))
+}
+
+fn bar(to: Path<HelloPath>) -> impl Responder {
+    format!("Hello {}!", &to.name)
 }
 
 fn main() {
     server::new(|| {
         App::new()
-            .resource("/", |r| r.f(hello))
-            .resource("/{name}", |r| r.f(hello))
+            .resource("/foo/{name}", |r| r.f(foo))
+            .resource("/bar/{name}", |r| r.with(bar))
     })
     .bind("localhost:3000")
     .expect("Can not bind to port 3000")
