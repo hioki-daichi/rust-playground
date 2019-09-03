9eb2ec9f9c0a3f5213698c81f7353daa15ee4213
Daichi Hioki <hiokidaichi@gmail.com>
2019-09-03 21:49:22 +0900

    Update

--- start-aw/src/main.rs
+++ start-aw/src/main.rs
@@ -1,12 +1,18 @@
-use actix_web::{server, App, HttpRequest, Responder};
+use actix_web::{server, App, Error, FromRequest, HttpRequest, Path};
+use serde_derive::*;
 
-fn hello_name(req: &HttpRequest) -> impl Responder {
-    let to = req.match_info().get("name").unwrap_or("World!");
-    format!("Hello, {}", to)
+#[derive(Deserialize)]
+struct HelloPath {
+    name: String,
+}
+
+fn hello_name(req: &HttpRequest) -> Result<String, Error> {
+    let to = Path::<HelloPath>::extract(req)?;
+    Ok(format!("Hello, {}!", &to.name))
 }
 
 fn main() {
-    server::new(|| App::new().resource("/", |r| r.f(hello_name)))
+    server::new(|| App::new().resource("/{name}", |r| r.f(hello_name)))
         .bind("127.0.0.1:59090")
         .unwrap()
         .run();
