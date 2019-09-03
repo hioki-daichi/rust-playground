033a14fde74ddfaa78556ddd1b6c26b4ab912e61
Daichi Hioki <hiokidaichi@gmail.com>
2019-09-03 20:38:48 +0900

    Update

--- start-aw/src/main.rs
+++ start-aw/src/main.rs
@@ -1,7 +1,12 @@
-use actix_web::{server, App, HttpResponse};
+use actix_web::{server, App, HttpRequest, Responder};
+
+fn hello_name(req: &HttpRequest) -> impl Responder {
+    let to = req.match_info().get("name").unwrap_or("World!");
+    format!("Hello, {}", to)
+}
 
 fn main() {
-    server::new(|| App::new().resource("/", |r| r.f(|_| HttpResponse::Ok())))
+    server::new(|| App::new().resource("/", |r| r.f(hello_name)))
         .bind("127.0.0.1:59090")
         .unwrap()
         .run();
