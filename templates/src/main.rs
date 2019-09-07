2e7ca0425c831be557f3a511924ae5e89c0f8acb
Daichi Hioki <hiokidaichi@gmail.com>
2019-09-07 11:37:24 +0900

    Update

--- templates/src/main.rs
+++ templates/src/main.rs
@@ -1,9 +1,35 @@
-use tera::Tera;
+use serde_derive::*;
 
 struct AppState {
-    template: Tera,
+    template: tera::Tera,
+}
+
+#[derive(Deserialize)]
+struct HelloPath {
+    name: String,
+}
+
+fn handler(
+    app: actix_web::State<AppState>,
+    path: actix_web::Path<HelloPath>,
+) -> Result<actix_web::HttpResponse, actix_web::error::Error> {
+    let mut ctx = tera::Context::new();
+    ctx.insert("name", &path.name);
+    let body = app
+        .template
+        .render("index.html.tera", &ctx)
+        .map_err(|e| actix_web::error::ErrorInternalServerError(format!("{}", e)))?;
+    Ok(actix_web::HttpResponse::Ok().body(body))
 }
 
 fn main() {
-    println!("{:?}", 1);
+    actix_web::server::new(|| {
+        let app = AppState {
+            template: tera::compile_templates!("templates/**/*"),
+        };
+        actix_web::App::with_state(app).route("/{name}", actix_web::http::Method::GET, handler)
+    })
+    .bind("localhost:3000")
+    .unwrap()
+    .run()
 }
