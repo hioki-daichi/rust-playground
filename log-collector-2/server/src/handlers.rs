c476272f0b28fe2d8972167cb1ffb1ccefaea5b7
Daichi Hioki <hiokidaichi@gmail.com>
2019-09-10 20:57:30 +0900

    Update

--- log-collector-2/server/src/handlers.rs
+++ log-collector-2/server/src/handlers.rs
@@ -5,3 +5,21 @@ pub fn handle_post_csv(
 ) -> Result<actix_web::HttpResponse, failure::Error> {
     unimplemented!()
 }
+
+pub fn handle_post_logs(
+    _server: actix_web::State<Server>,
+) -> Result<actix_web::HttpResponse, failure::Error> {
+    unimplemented!()
+}
+
+pub fn handle_get_csv(
+    _server: actix_web::State<Server>,
+) -> Result<actix_web::HttpResponse, failure::Error> {
+    unimplemented!()
+}
+
+pub fn handle_get_logs(
+    _server: actix_web::State<Server>,
+) -> Result<actix_web::HttpResponse, failure::Error> {
+    unimplemented!()
+}
--- log-collector-2/server/src/main.rs
+++ log-collector-2/server/src/main.rs
@@ -15,11 +15,27 @@ fn main() {
     env_logger::init();
 
     actix_web::server::new(|| {
-        actix_web::App::with_state(Server::new()).route(
-            "/csv",
-            actix_web::http::Method::POST,
-            handlers::handle_post_csv,
-        )
+        actix_web::App::with_state(Server::new())
+            .route(
+                "/csv",
+                actix_web::http::Method::POST,
+                handlers::handle_post_csv,
+            )
+            .route(
+                "/logs",
+                actix_web::http::Method::POST,
+                handlers::handle_post_logs,
+            )
+            .route(
+                "/csv",
+                actix_web::http::Method::GET,
+                handlers::handle_get_csv,
+            )
+            .route(
+                "/logs",
+                actix_web::http::Method::GET,
+                handlers::handle_get_logs,
+            )
     })
     .bind("localhost:3000")
     .unwrap()
