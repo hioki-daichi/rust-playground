3a0300d4e2c97de5927de960692bafcf88acf9c0
Daichi Hioki <hiokidaichi@gmail.com>
2019-09-10 21:24:11 +0900

    Update

--- log-collector-2/api/src/lib.rs
+++ log-collector-2/api/src/lib.rs
@@ -1,5 +1,23 @@
+use serde_derive::*;
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash, Deserialize, Serialize)]
+pub struct Log {
+    pub user_agent: String,
+    pub response_time: i32,
+    pub timestamp: chrono::DateTime<chrono::Utc>,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash, Deserialize, Serialize)]
+pub struct DateTimeRange {
+    pub from: Option<chrono::DateTime<chrono::Utc>>,
+    pub until: Option<chrono::DateTime<chrono::Utc>>,
+}
+
 pub mod logs {
-    pub mod get {}
+    pub mod get {
+        pub type Query = crate::DateTimeRange;
+        pub struct Response(pub Vec<crate::Log>);
+    }
 
     pub mod post {}
 }
