7bcd6345971107265e6c7e8d6f8fcf7e695c9d00
Daichi Hioki <hiokidaichi@gmail.com>
2019-09-10 22:41:08 +0900

    Update

--- log-collector-2/server/src/handlers.rs
+++ log-collector-2/server/src/handlers.rs
@@ -3,23 +3,46 @@ use crate::Server;
 pub fn handle_post_csv(
     _server: actix_web::State<Server>,
 ) -> Result<actix_web::HttpResponse, failure::Error> {
-    unimplemented!()
+    // TODO: POST されたファイルを扱う
+
+    // ダミーデータ
+    let logs = Default::default();
+
+    let resp = api::csv::post::Response(logs);
+
+    Ok(actix_web::HttpResponse::Ok().json(resp))
 }
 
 pub fn handle_post_logs(
     _server: actix_web::State<Server>,
+    log: actix_web::Json<api::logs::post::Request>,
 ) -> Result<actix_web::HttpResponse, failure::Error> {
-    unimplemented!()
-}
-
-pub fn handle_get_csv(
-    _server: actix_web::State<Server>,
-) -> Result<actix_web::HttpResponse, failure::Error> {
-    unimplemented!()
+    log::debug!("{:?}", log);
+    Ok(actix_web::HttpResponse::Accepted().finish())
 }
 
 pub fn handle_get_logs(
     _server: actix_web::State<Server>,
+    range: actix_web::Query<api::logs::get::Query>,
 ) -> Result<actix_web::HttpResponse, failure::Error> {
-    unimplemented!()
+    log::debug!("{:?}", range);
+
+    let logs = Default::default();
+
+    let resp = api::logs::get::Response(logs);
+
+    Ok(actix_web::HttpResponse::Ok().json(resp))
+}
+
+pub fn handle_get_csv(
+    _server: actix_web::State<Server>,
+    range: actix_web::Query<api::csv::get::Query>,
+) -> Result<actix_web::HttpResponse, failure::Error> {
+    log::debug!("{:?}", range);
+
+    let csv: Vec<u8> = vec![];
+
+    Ok(actix_web::HttpResponse::Ok()
+        .header("Content-Type", "text/csv")
+        .body(csv))
 }
