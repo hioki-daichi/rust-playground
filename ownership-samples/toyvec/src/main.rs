e7e9445ca89e57aeac1c7d09f66f1e01c687cf29
Daichi Hioki <hiokidaichi@gmail.com>
2019-09-17 23:36:03 +0900

    Update

--- ownership-samples/toyvec/src/main.rs
+++ ownership-samples/toyvec/src/main.rs
@@ -73,51 +73,90 @@ impl<T: Default> ToyVec<T> {
             Some(elem)
         }
     }
+
+    // 本来ならライフタイム指定子を書く必要はないが、
+    // self と戻り値 Iter のライフタイムが `'vec` で同じであることを明示的に書くことで
+    // Iter が生存している間は ToyVec が借用されていることを説明している。
+    pub fn iter<'vec>(&'vec self) -> Iter<'vec, T> {
+        Iter {
+            elements: &self.elements,
+            len: self.len,
+            pos: 0,
+        }
+    }
+}
+
+pub struct Iter<'vec, T> {
+    elements: &'vec Box<[T]>,
+    len: usize,
+    pos: usize,
+}
+
+impl<'vec, T> Iterator for Iter<'vec, T> {
+    type Item = &'vec T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.pos >= self.len {
+            None
+        } else {
+            let res = Some(&self.elements[self.pos]);
+            self.pos += 1;
+            res
+        }
+    }
 }
 
 fn main() {
+    // let mut v = ToyVec::new();
+    // println!("{:?}", v);
+    // v.push("foo".to_string());
+    // println!("{:?}", v);
+    // v.push("bar".to_string());
+    // println!("{:?}", v);
+    // v.push("baz".to_string());
+    // println!("{:?}", v);
+    // v.push("qux".to_string());
+    // println!("{:?}", v);
+    // v.push("quux".to_string());
+    // println!("{:?}", v);
+    // v.push("corge".to_string());
+    // println!("{:?}", v);
+    // v.push("grault".to_string());
+    // println!("{:?}", v);
+    // v.push("garply".to_string());
+    // println!("{:?}", v);
+    // v.push("waldo".to_string());
+    // println!("{:?}", v);
+    // v.push("fred".to_string());
+    // println!("{:?}", v);
+    // v.push("plugh".to_string());
+    // println!("{:?}", v);
+    // v.push("xyzzy".to_string());
+    // println!("{:?}", v);
+    // v.push("thud".to_string());
+    // println!("{:?}", v);
+    // // len: 0, capacity: 0, elements: []
+    // // len: 1, capacity: 1, elements: ["foo"]
+    // // len: 2, capacity: 2, elements: ["foo", "bar"]
+    // // len: 3, capacity: 4, elements: ["foo", "bar", "baz", ""]
+    // // len: 4, capacity: 4, elements: ["foo", "bar", "baz", "qux"]
+    // // len: 5, capacity: 8, elements: ["foo", "bar", "baz", "qux", "quux", "", "", ""]
+    // // len: 6, capacity: 8, elements: ["foo", "bar", "baz", "qux", "quux", "corge", "", ""]
+    // // len: 7, capacity: 8, elements: ["foo", "bar", "baz", "qux", "quux", "corge", "grault", ""]
+    // // len: 8, capacity: 8, elements: ["foo", "bar", "baz", "qux", "quux", "corge", "grault", "garply"]
+    // // len: 9, capacity: 16, elements: ["foo", "bar", "baz", "qux", "quux", "corge", "grault", "garply", "waldo", "", "", "", "", "", "", ""]
+    // // len: 10, capacity: 16, elements: ["foo", "bar", "baz", "qux", "quux", "corge", "grault", "garply", "waldo", "fred", "", "", "", "", "", ""]
+    // // len: 11, capacity: 16, elements: ["foo", "bar", "baz", "qux", "quux", "corge", "grault", "garply", "waldo", "fred", "plugh", "", "", "", "", ""]
+    // // len: 12, capacity: 16, elements: ["foo", "bar", "baz", "qux", "quux", "corge", "grault", "garply", "waldo", "fred", "plugh", "xyzzy", "", "", "", ""]
+    // // len: 13, capacity: 16, elements: ["foo", "bar", "baz", "qux", "quux", "corge", "grault", "garply", "waldo", "fred", "plugh", "xyzzy", "thud", "", "", ""]
+
     let mut v = ToyVec::new();
-    println!("{:?}", v);
-    v.push("foo".to_string());
-    println!("{:?}", v);
-    v.push("bar".to_string());
-    println!("{:?}", v);
-    v.push("baz".to_string());
-    println!("{:?}", v);
-    v.push("qux".to_string());
-    println!("{:?}", v);
-    v.push("quux".to_string());
-    println!("{:?}", v);
-    v.push("corge".to_string());
-    println!("{:?}", v);
-    v.push("grault".to_string());
-    println!("{:?}", v);
-    v.push("garply".to_string());
-    println!("{:?}", v);
-    v.push("waldo".to_string());
-    println!("{:?}", v);
-    v.push("fred".to_string());
-    println!("{:?}", v);
-    v.push("plugh".to_string());
-    println!("{:?}", v);
-    v.push("xyzzy".to_string());
-    println!("{:?}", v);
-    v.push("thud".to_string());
-    println!("{:?}", v);
-    // len: 0, capacity: 0, elements: []
-    // len: 1, capacity: 1, elements: ["foo"]
-    // len: 2, capacity: 2, elements: ["foo", "bar"]
-    // len: 3, capacity: 4, elements: ["foo", "bar", "baz", ""]
-    // len: 4, capacity: 4, elements: ["foo", "bar", "baz", "qux"]
-    // len: 5, capacity: 8, elements: ["foo", "bar", "baz", "qux", "quux", "", "", ""]
-    // len: 6, capacity: 8, elements: ["foo", "bar", "baz", "qux", "quux", "corge", "", ""]
-    // len: 7, capacity: 8, elements: ["foo", "bar", "baz", "qux", "quux", "corge", "grault", ""]
-    // len: 8, capacity: 8, elements: ["foo", "bar", "baz", "qux", "quux", "corge", "grault", "garply"]
-    // len: 9, capacity: 16, elements: ["foo", "bar", "baz", "qux", "quux", "corge", "grault", "garply", "waldo", "", "", "", "", "", "", ""]
-    // len: 10, capacity: 16, elements: ["foo", "bar", "baz", "qux", "quux", "corge", "grault", "garply", "waldo", "fred", "", "", "", "", "", ""]
-    // len: 11, capacity: 16, elements: ["foo", "bar", "baz", "qux", "quux", "corge", "grault", "garply", "waldo", "fred", "plugh", "", "", "", "", ""]
-    // len: 12, capacity: 16, elements: ["foo", "bar", "baz", "qux", "quux", "corge", "grault", "garply", "waldo", "fred", "plugh", "xyzzy", "", "", "", ""]
-    // len: 13, capacity: 16, elements: ["foo", "bar", "baz", "qux", "quux", "corge", "grault", "garply", "waldo", "fred", "plugh", "xyzzy", "thud", "", "", ""]
+    v.push("Java Finch".to_string());
+    v.push("Budgerigar".to_string());
+    let mut iter = v.iter();
+    // v.push("Hill Mynah".to_string()); // コンパイルエラー
+    assert_eq!(iter.next(), Some(&"Java Finch".to_string()));
+    v.push("Canary".to_string());
 }
 
 // capacity(&self) も都度しりたいため #[derive(Debug)] せずに自分で書く
