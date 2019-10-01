a0c8badd4f82f53149725f92640745f294bc870d
Daichi Hioki <hiokidaichi@gmail.com>
2019-10-02 07:23:02 +0900

    Update

--- tokio-rs-sample/src/main.rs
+++ tokio-rs-sample/src/main.rs
@@ -1,31 +1,35 @@
 use tokio::net::TcpListener;
 use tokio::prelude::*;
+use tokio::runtime::Runtime;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut listener = TcpListener::bind("127.0.0.1:8080").await?;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let rt = Runtime::new()?;
 
-    loop {
-        let (mut socket, _) = listener.accept().await?;
+    rt.block_on(async {
+        let mut listener = TcpListener::bind("127.0.0.1:8080").await?;
 
-        tokio::spawn(async move {
-            let mut buf = [0; 1024];
+        loop {
+            let (mut socket, _) = listener.accept().await?;
 
-            loop {
-                let n = match socket.read(&mut buf).await {
-                    Ok(n) if n == 0 => return,
-                    Ok(n) => n,
-                    Err(e) => {
+            tokio::spawn(async move {
+                let mut buf = [0; 1024];
+
+                loop {
+                    let n = match socket.read(&mut buf).await {
+                        Ok(n) if n == 0 => return,
+                        Ok(n) => n,
+                        Err(e) => {
+                            println!("{:?}", e);
+                            return;
+                        }
+                    };
+
+                    if let Err(e) = socket.write_all(&buf[0..n]).await {
                         println!("{:?}", e);
                         return;
                     }
-                };
-
-                if let Err(e) = socket.write_all(&buf[0..n]).await {
-                    println!("{:?}", e);
-                    return;
                 }
-            }
-        });
-    }
+            });
+        }
+    })
 }
