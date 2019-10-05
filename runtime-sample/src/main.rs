e1322f77fa91e1e551b0240ee8124e0eb5d55bd1
Daichi Hioki <hiokidaichi@gmail.com>
2019-10-05 23:41:17 +0900

    Update

--- runtime-sample/src/main.rs
+++ runtime-sample/src/main.rs
@@ -1,15 +1,27 @@
+use actix_web::client::Client;
 use actix_web::*;
 use futures::future::ok;
 use futures::Future;
 
 fn main() {
-    HttpServer::new(|| App::new().route("/", web::get().to_async(index)))
+    HttpServer::new(|| App::new().route("/", web::get().to_async(handler)))
         .bind("127.0.0.1:8080")
         .unwrap()
         .run()
         .unwrap()
 }
 
-fn index() -> impl Future<Item = HttpResponse, Error = actix_web::Error> {
-    ok::<_, Error>(HttpResponse::Ok().content_type("text/html").body("Hello!"))
+fn handler() -> impl Future<Item = HttpResponse, Error = ()> {
+    Client::default()
+        .get("https://httpbin.org/get")
+        .send()
+        .map_err(|e| {
+            println!("{:?}", e);
+            ()
+        })
+        .and_then(|mut response| {
+            ok(HttpResponse::Ok()
+                .content_type("text/html")
+                .body(String::from_utf8(response.body().wait().unwrap().to_vec()).unwrap()))
+        })
 }
