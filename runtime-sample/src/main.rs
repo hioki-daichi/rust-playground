c6d98fb1b43408cc89c287fe90805dac6ed49367
Daichi Hioki <hiokidaichi@gmail.com>
2019-10-05 23:24:05 +0900

    Update

--- runtime-sample/src/main.rs
+++ runtime-sample/src/main.rs
@@ -1,13 +1,17 @@
 use actix_web::*;
+use futures::future::ok;
+use futures::Future;
 
 fn main() {
-    HttpServer::new(|| App::new().route("/", web::get().to(index)))
+    HttpServer::new(|| App::new().route("/", web::get().to_async(index)))
         .bind("127.0.0.1:8080")
         .unwrap()
         .run()
         .unwrap()
 }
 
-fn index() -> impl Responder {
-    "Welcome!".with_status(http::StatusCode::OK)
+fn index() -> Box<dyn Future<Item = HttpResponse, Error = actix_web::Error>> {
+    Box::new(ok::<_, Error>(
+        HttpResponse::Ok().content_type("text/html").body("Hello!"),
+    ))
 }
