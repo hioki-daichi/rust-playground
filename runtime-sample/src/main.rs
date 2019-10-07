adf4c120b0f31ce22064414908f65d76d75e4e75
Daichi Hioki <hiokidaichi@gmail.com>
2019-10-07 20:09:18 +0900

    Update

--- runtime-sample/src/main.rs
+++ runtime-sample/src/main.rs
@@ -1,5 +1,6 @@
 use actix_web::client::Client;
 use actix_web::*;
+use failure::{Error, Fail};
 use futures::future::ok;
 use futures::{future::Either, Future};
 use serde_derive::*;
@@ -30,17 +31,19 @@ fn main() {
 fn handler(
     config: web::Data<Config>,
     info: web::Path<Info>,
-) -> impl Future<Item = HttpResponse, Error = ()> {
+) -> impl Future<Item = HttpResponse, Error = Error> {
     let url = format!("{}/{}.keys", config.github_url, info.username);
 
     let foo = if true {
         Either::A(
             send_request(url)
+                .map_err(|e| MyError::A(format!("A: {}", e)).into())
                 .and_then(|body| ok(HttpResponse::Ok().content_type("text/html").body(body))),
         )
     } else {
         Either::B(
             send_request(url)
+                .map_err(|e| MyError::B(format!("B: {}", e)).into())
                 .and_then(|body| ok(HttpResponse::Ok().content_type("text/plain").body(body))),
         )
     };
@@ -48,15 +51,24 @@ fn handler(
     foo
 }
 
-fn send_request(url: String) -> impl Future<Item = String, Error = ()> {
+fn send_request(url: String) -> impl Future<Item = String, Error = Error> {
     Client::default()
         .get(url)
         .send()
-        .map_err(|e| {
-            println!("{:?}", e);
-            ()
-        })
+        .map_err(|e| MyError::C(format!("{}", e)).into())
         .and_then(|mut response| {
             ok(String::from_utf8(response.body().wait().unwrap().to_vec()).unwrap())
         })
+}
+
+#[derive(Debug, Fail)]
+enum MyError {
+    #[fail(display = "A: {}", _0)]
+    A(String),
+
+    #[fail(display = "B: {}", _0)]
+    B(String),
+
+    #[fail(display = "C: {}", _0)]
+    C(String),
 }
