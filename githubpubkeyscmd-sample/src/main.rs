d7ad3fedba5c48d2a7dddb4a6bb0d4518ce8a1af
Daichi Hioki <hiokidaichi@gmail.com>
2019-10-08 22:18:14 +0900

    Update

--- githubpubkeyscmd-sample/src/main.rs
+++ githubpubkeyscmd-sample/src/main.rs
@@ -1,24 +1,23 @@
 use actix_rt::System;
 use actix_web::client::Client;
 use failure::{Error, Fail};
-use futures::{future::ok, future::result, lazy, Future};
+use futures::{future::ok, lazy, Future};
 
 fn main() {
-    System::new("test")
-        .block_on(lazy(|| {
-            let foo = if true { send_request() } else { send_request() };
-            foo.and_then(|response_body| {
-                println!("{:?}", response_body);
-                ok(())
-            })
-            .wait()
-        }))
+    f();
+}
+
+fn f() -> impl Future<Item = String, Error = Error> {
+    let response_body = System::new("test")
+        .block_on(lazy(|| if true { send_request() } else { send_request() }))
         .unwrap();
+    println!("{:?}", response_body);
+    ok(response_body)
 }
 
 fn send_request() -> impl Future<Item = String, Error = Error> {
     Client::default()
-        .get("https://github.com/hioki-daichi")
+        .get("https://github.com/hioki-daichi.keys")
         .send()
         .map_err(|e| MyError::A(format!("{}", e)).into())
         .and_then(|mut response| {
