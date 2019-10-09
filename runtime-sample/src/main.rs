7d215db715479969d94392f38933e4322f281001
Daichi Hioki <hiokidaichi@gmail.com>
2019-10-09 21:53:20 +0900

    Update

--- runtime-sample/src/main.rs
+++ runtime-sample/src/main.rs
@@ -2,7 +2,7 @@ use actix_web::client::Client;
 use actix_web::*;
 use failure::{Error, Fail};
 use futures::future::ok;
-use futures::{future::Either, Future};
+use futures::{future::err, future::Either, Future};
 use serde_derive::*;
 
 #[derive(Deserialize)]
@@ -34,28 +34,30 @@ fn handler(
 ) -> impl Future<Item = HttpResponse, Error = Error> {
     let url = format!("{}/{}.keys", config.github_url, info.username);
 
-    let foo = if true {
-        Either::A(
-            send_request(url)
-                .map_err(|e| MyError::A(format!("A: {}", e)).into())
-                .and_then(|body| ok(HttpResponse::Ok().content_type("text/html").body(body))),
-        )
+    if true {
+        Either::A(if true {
+            Either::A(
+                send_request(url)
+                    .map_err(|_e| MyError::A().into())
+                    .and_then(|body| ok(HttpResponse::Ok().body(body))),
+            )
+        } else {
+            Either::B(
+                send_request(url)
+                    .map_err(|_e| MyError::B().into())
+                    .and_then(|_body| ok(HttpResponse::Ok().body("unreachable"))),
+            )
+        })
     } else {
-        Either::B(
-            send_request(url)
-                .map_err(|e| MyError::B(format!("B: {}", e)).into())
-                .and_then(|body| ok(HttpResponse::Ok().content_type("text/plain").body(body))),
-        )
-    };
-
-    foo
+        Either::B(err(MyError::A().into()))
+    }
 }
 
 fn send_request(url: String) -> impl Future<Item = String, Error = Error> {
     Client::default()
         .get(url)
         .send()
-        .map_err(|e| MyError::C(format!("{}", e)).into())
+        .map_err(|_e| MyError::C().into())
         .and_then(|mut response| {
             ok(String::from_utf8(response.body().wait().unwrap().to_vec()).unwrap())
         })
@@ -63,12 +65,12 @@ fn send_request(url: String) -> impl Future<Item = String, Error = Error> {
 
 #[derive(Debug, Fail)]
 enum MyError {
-    #[fail(display = "A: {}", _0)]
-    A(String),
+    #[fail(display = "A")]
+    A(),
 
-    #[fail(display = "B: {}", _0)]
-    B(String),
+    #[fail(display = "B")]
+    B(),
 
-    #[fail(display = "C: {}", _0)]
-    C(String),
+    #[fail(display = "C")]
+    C(),
 }
