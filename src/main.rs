5295b956e989f23a56dd2831106df85704eabd3f
Daichi Hioki <hiokidaichi@gmail.com>
2019-10-19 23:11:49 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,15 @@ fn bar<'a, 'b>(x: &'a i32, y: &'b i32) {
     println!("{:?} {:?}", x, y);
 }
 
+fn baz<'a, 'b>(x: &'a mut i32, y: &'b i32) -> i32 {
+    *x += y;
+    *x
+}
+
 fn main() {
     foo(&1, &2);
     bar(&1, &2);
+    let mut a: i32 = 100;
+    baz(&mut a, &5);
+    println!("{:?}", a);
 }
