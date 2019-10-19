a58f3014821535b7553ae128226d6ac4c5780be3
Daichi Hioki <hiokidaichi@gmail.com>
2019-10-19 09:52:49 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("{:?}", 1);
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+    let result = longest(string1.as_str(), string2);
+    println!("{:?}", result);
+}
+
+fn longest<'a>(a: &'a str, b: &'a str) -> &'a str {
+    if a.len() > b.len() {
+        a
+    } else {
+        b
+    }
 }
