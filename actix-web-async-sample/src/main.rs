f5fe2f0e5c6a38ccfeb6f7f432bddaf7016e3a8f
Daichi Hioki <hiokidaichi@gmail.com>
2019-11-28 07:34:19 +0900

    Update

--- actix-web-async-sample/src/main.rs
+++ actix-web-async-sample/src/main.rs
@@ -1,8 +1,7 @@
 use actix_web::*;
-use futures::Future;
 
 fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(web::resource("/").route(web::get().to_async(handler))))
+    HttpServer::new(|| App::new().service(web::resource("/").route(web::get().to(handler))))
         .bind("127.0.0.1:8080")?
         .run()
 }
@@ -12,13 +11,14 @@ struct Foo<'a> {
 }
 
 impl<'a> Foo<'a> {
-    fn bar(&self) -> impl Future<Item = String, Error = ()> {
-        futures::future::ok(format!("{}{}", self.a, "Z"))
+    async fn bar(&self) -> Result<String, ()> {
+        Ok(format!("{}{}", self.a, "Z"))
     }
 }
 
-fn handler() -> impl Future<Item = HttpResponse, Error = ()> {
+async fn handler() -> Result<HttpResponse, ()> {
     let foo = Foo { a: "A" };
     foo.bar()
+        .await
         .map(|s| HttpResponse::Ok().content_type("text/plain").body(s))
 }
