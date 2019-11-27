bfbd4f1b9a3b0808fb5690e8d2fd4020c3f0d651
Daichi Hioki <hiokidaichi@gmail.com>
2019-11-28 06:19:09 +0900

    Update

--- futures-sample/src/main.rs
+++ futures-sample/src/main.rs
@@ -1,26 +1,32 @@
-use failure::{err_msg, Error, Fail};
-use futures::Future;
+use failure::{format_err, Error, Fail};
 
 fn main() {
-    println!("{:?}", f(100).wait());
+    let fut = f(100);
+    println!("{:?}", futures::executor::block_on(fut));
+}
+
+async fn f(a: u32) -> Result<u32, Error> {
+    let b = add1(a).await?;
+    let c = mul3(b).await?;
+    cause_err(c).await
+}
+
+async fn add1(a: u32) -> Result<u32, Error> {
+    Ok(a + 1)
+}
+
+async fn mul3(a: u32) -> Result<u32, Error> {
+    Ok(a * 3)
 }
 
 #[derive(Debug, Fail)]
 #[fail(display = "my wrapping error")]
 struct WrappingError(#[fail(cause)] Error);
 
-fn f(n: u32) -> impl Future<Item = u32, Error = Error> {
-    add1(n).and_then(|b| mul2(b))
-}
-
-fn add1(n: u32) -> impl Future<Item = u32, Error = Error> {
-    futures::future::ok(n + 1)
-}
-
-fn mul2(n: u32) -> impl Future<Item = u32, Error = Error> {
-    futures::future::ok(n * 2)
-}
-
-fn cause_err(s: String) -> impl Future<Item = u32, Error = Error> {
-    futures::future::err(failure::Error { WrappingError.into() })
+async fn cause_err(a: u32) -> Result<u32, Error> {
+    if a % 2 == 0 {
+        Err(format_err!("This is even! {:?}", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
+    } else {
+        Ok(a)
+    }
 }
