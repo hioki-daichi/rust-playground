c3abdb03bd02852060ec0bc98ece6ec5165990eb
Daichi Hioki <hiokidaichi@gmail.com>
2019-12-03 08:25:48 +0900

    Update

--- yew-file-upload-sample/src/main.rs
+++ yew-file-upload-sample/src/main.rs
@@ -1,3 +1,28 @@
+use yew::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<Model>();
+}
+
+struct Model {}
+
+enum Msg {}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        Model {}
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        true
+    }
+
+    fn view(&self) -> Html<Self> {
+        html! {
+            <strong>{{ "Hello, World!" }}</strong>
+        }
+    }
 }
