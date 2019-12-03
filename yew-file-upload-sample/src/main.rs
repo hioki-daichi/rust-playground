fd1b9059e5fb3b485b2503f93e2f73e389ecd9f0
Daichi Hioki <hiokidaichi@gmail.com>
2019-12-03 21:44:03 +0900

    Output the content of the file

--- yew-file-upload-sample/src/main.rs
+++ yew-file-upload-sample/src/main.rs
@@ -1,28 +1,71 @@
+use yew::html;
 use yew::prelude::*;
+use yew::services::reader::{FileData, ReaderService, ReaderTask};
+use yew::services::ConsoleService;
+use yew::ChangeData;
 
 fn main() {
     yew::start_app::<Model>();
 }
 
-struct Model {}
+struct Model {
+    link: ComponentLink<Self>,
+    reader: ReaderService,
+    reader_tasks: Vec<ReaderTask>,
+    console: ConsoleService,
+}
 
-enum Msg {}
+enum Msg {
+    ChooseFile(ChangeData),
+    LoadedFile(FileData),
+}
 
 impl Component for Model {
     type Message = Msg;
     type Properties = ();
 
-    fn create(_: Self::Properties, _link: ComponentLink<Self>) -> Self {
-        Model {}
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self {
+            link,
+            reader: ReaderService::new(),
+            reader_tasks: vec![],
+            console: ConsoleService::new(),
+        }
     }
 
-    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::ChooseFile(ChangeData::Files(files)) => {
+                for file in files {
+                    self.reader_tasks.push(
+                        self.reader
+                            .read_file(file, self.link.send_back(move |v| Msg::LoadedFile(v))),
+                    );
+                }
+            }
+
+            Msg::ChooseFile(ChangeData::Value(_)) => {
+                self.console.log("ChangeData::Value");
+            }
+
+            Msg::ChooseFile(ChangeData::Select(_)) => {
+                self.console.log("ChangeData::Select");
+            }
+
+            Msg::LoadedFile(file_data) => {
+                self.console
+                    .log(format!("{:?}", file_data.content).as_str());
+            }
+        }
+
         true
     }
 
     fn view(&self) -> Html<Self> {
         html! {
-            <strong>{{ "Hello, World!" }}</strong>
+            <div>
+                <input type="file" onchange = |change_data| Msg::ChooseFile(change_data) />
+            </div>
         }
     }
 }
