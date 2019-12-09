e7781aeb67da5b93b411b25c43e66e0d82d1de6b
Daichi Hioki <hiokidaichi@gmail.com>
2019-12-09 23:25:43 +0900

    Update

--- yew-sample/src/main.rs
+++ yew-sample/src/main.rs
@@ -1,110 +1,58 @@
 #![recursion_limit = "256"]
 
-#[macro_use]
-extern crate stdweb;
-
-use stdweb::unstable::TryFrom;
-use stdweb::web::Node;
-use yew::format::{Nothing, Text};
 use yew::prelude::*;
-use yew::services::{
-    fetch::{FetchTask, Request, Response},
-    ConsoleService, FetchService,
-};
-use yew::virtual_dom::VNode;
+use yew::services::ConsoleService;
 
 fn main() {
-    yew::initialize();
-    App::<Model>::new().mount_to_body();
-    yew::run_loop();
+    yew::start_app::<Model>();
 }
 
+#[derive(Debug)]
+struct A {
+    a: u32,
+}
+
+#[derive(Debug)]
 struct Model {
-    fetch_service: FetchService,
+    xs: Vec<A>,
     console: ConsoleService,
-    link: ComponentLink<Model>,
-    ft: Option<FetchTask>,
-    svg: String,
-    username: String,
-    is_loading: bool,
 }
 
 enum Msg {
-    SendRequest,
-    FetchReady(String),
-    Ignore,
-    EditUsername(String),
+    X,
+    Y,
 }
 
 impl Component for Model {
     type Message = Msg;
     type Properties = ();
 
-    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+    fn create(_: Self::Properties, _link: ComponentLink<Self>) -> Self {
         Model {
-            fetch_service: FetchService::new(),
-            link,
+            xs: vec![],
             console: ConsoleService::new(),
-            ft: None,
-            svg: String::from(""),
-            username: String::from(""),
-            is_loading: false,
         }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::SendRequest => {
-                self.is_loading = true;
-                let callback = self.link.send_back(move |response: Response<Text>| {
-                    let (parts, body) = response.into_parts();
-                    if parts.status.is_success() {
-                        Msg::FetchReady(body.unwrap())
-                    } else {
-                        Msg::Ignore
-                    }
-                });
-                let request = Request::get(format!("http://127.0.0.1:8080/{}", self.username))
-                    .body(Nothing)
-                    .unwrap();
-                self.ft = Some(self.fetch_service.fetch(request, callback));
+            Msg::X => {
+                self.xs.insert(0, A { a: 1 });
+                self.console.log(format!("{:?}", self.xs).as_str());
             }
-            Msg::FetchReady(body) => {
-                self.is_loading = false;
-                self.svg = body;
-            }
-            Msg::Ignore => {
-                self.is_loading = false;
-                self.svg = String::from("");
-            }
-            Msg::EditUsername(username) => {
-                self.username = username;
+            Msg::Y => {
+                self.xs.insert(0, A { a: 2 });
+                self.console.log(format!("{:?}", self.xs).as_str());
             }
         }
         true
     }
-}
 
-impl Renderable<Model> for Model {
     fn view(&self) -> Html<Self> {
-        let js_result = js! {
-          var element = document.createElement("div");
-          element.innerHTML = @{self.svg.as_str()};
-          return element;
-        };
-
-        let node = Node::try_from(js_result).expect("failed to convert js_result to node");
-
-        let vnode = VNode::VRef(node);
-
-        let loading_message = if self.is_loading { "loading..." } else { "" };
-
         html! {
           <div>
-            <input type="text" oninput=|e| Msg::EditUsername(e.value) />
-            <button class="send" onclick=|_| Msg::SendRequest>{ "Send" }</button>
-            <span class="loading">{loading_message}</span>
-            { vnode }
+              <button onclick=|_e| Msg::X>{{"XXXXXXXXXX"}}</button>
+              <button onclick=|_e| Msg::Y>{{"YYYYYYYYYYYY"}}</button>
           </div>
         }
     }
