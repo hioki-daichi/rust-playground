2a0993f0af7f8644de7057965d30f2b82227804a
Daichi Hioki <hiokidaichi@gmail.com>
2019-12-12 08:46:48 +0900

    Update

--- rusoto-sample/src/main.rs
+++ rusoto-sample/src/main.rs
@@ -1,20 +1,49 @@
+#[allow(unused_imports)]
+use rusoto_core::credential::{AwsCredentials, DefaultCredentialsProvider, ProvideAwsCredentials};
 use rusoto_core::{ByteStream, Region};
+use rusoto_s3::util::{PreSignedRequest, PreSignedRequestOption};
 use rusoto_s3::{GetObjectRequest, PutObjectRequest, S3Client, S3};
 use std::fs::File;
 
 fn main() {
+    #[allow(unused_variables)]
     let client = build_client();
     // print_object(&client, "sample1.mp4".to_owned());
-    put_object(&client);
+    // put_object(&client);
+    let url = presigned_url("sample1.mp4".to_owned());
+    println!("{:?}", url);
 }
 
 fn build_client() -> S3Client {
-    S3Client::new(Region::Custom {
-        name: "minio".to_owned(),
-        endpoint: "http://localhost:9001".to_owned(),
-    })
+    S3Client::new(build_region())
 }
 
+fn build_region() -> Region {
+    Region::Custom {
+        name: "minio".to_owned(),
+        endpoint: "http://localhost:9001".to_owned(),
+    }
+}
+
+fn presigned_url(key: String) -> String {
+    let get_obj_req = GetObjectRequest {
+        bucket: "videos".to_owned(),
+        key: key,
+        ..Default::default()
+    };
+
+    let option = PreSignedRequestOption {
+        expires_in: ::std::time::Duration::from_secs(60),
+    };
+
+    let region = build_region();
+
+    let credentials = AwsCredentials::new("minio", "minio123", None, None);
+
+    get_obj_req.get_presigned_url(&region, &credentials, &option)
+}
+
+#[allow(dead_code)]
 fn put_object(client: &S3Client) {
     let mut file = File::open("sample1.mp4").unwrap();
     let mut buf = Vec::new();
