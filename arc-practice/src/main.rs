6c4ac1e2252e11cf767ff32b7d16f7ffa3eab4be
Daichi Hioki <hiokidaichi@gmail.com>
2019-12-31 00:11:31 +0900

    Update

--- arc-practice/src/main.rs
+++ arc-practice/src/main.rs
@@ -1,50 +1,38 @@
+use std::cell::RefCell;
 use std::rc::Rc;
 
-pub struct Stack<T>(Option<Rc<(T, Stack<T>)>>);
+struct Node {
+    name: &'static str,
+    neighbors: RefCell<Vec<Rc<Node>>>,
+}
 
-impl<T> Clone for Stack<T> {
-    fn clone(&self) -> Self {
-        Self(self.0.clone())
+impl Node {
+    fn new(name: &'static str) -> Rc<Self> {
+        Rc::new(Self {
+            name,
+            neighbors: RefCell::new(Vec::new()),
+        })
     }
 }
 
-impl<T> Stack<T> {
-    pub fn new() -> Self {
-        Self(None)
-    }
-
-    pub fn push(&mut self, x: T) {
-        let this = Self(self.0.take());
-        self.0 = Some(Rc::new((x, this)));
-    }
-
-    pub fn peek(&self) -> Option<&T> {
-        if let Some(rc) = &self.0 {
-            Some(&rc.0)
-        } else {
-            None
-        }
-    }
-}
-
-impl<T: Clone> Stack<T> {
-    pub fn pop(&mut self) -> Option<T> {
-        let this = Self(self.0.take());
-        if let Some(rc) = this.0 {
-            let (head, tail) = Rc::try_unwrap(rc).unwrap_or_else(|rc| (*rc).clone());
-            *self = tail;
-            Some(head)
-        } else {
-            None
-        }
+// 回収時に name を出力
+impl Drop for Node {
+    fn drop(&mut self) {
+        println!("{:?}", self.name);
     }
 }
 
 fn main() {
-    let mut s: Stack<i32> = Stack::new();
-    assert_eq!(s.peek(), None);
-    s.push(42);
-    assert_eq!(s.peek(), Some(&42));
-    assert_eq!(s.pop(), Some(42));
-    assert_eq!(s.peek(), None);
-}
+    let node1 = Node::new("node1");
+    let node2 = Node::new("node2");
+    let node3 = Node::new("node3");
+
+    // 1 -> 2
+    node1.neighbors.borrow_mut().push(node2.clone());
+
+    // 2 -> 1
+    node2.neighbors.borrow_mut().push(node1.clone());
+
+    // 3 -> 1
+    node3.neighbors.borrow_mut().push(node1.clone());
+} // 1 <-> 2 で循環参照しているため "node3" しか出力されない
