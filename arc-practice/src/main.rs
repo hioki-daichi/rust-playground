417fd6fae2cc18426762bcc235ee833a06ee4ba4
Daichi Hioki <hiokidaichi@gmail.com>
2019-12-30 23:20:09 +0900

    Update

--- arc-practice/src/main.rs
+++ arc-practice/src/main.rs
@@ -1,12 +1,31 @@
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::thread;
 
 fn main() {
-    let rc = Arc::new(42);
+    let counter = Arc::new(Mutex::new(0));
 
-    println!("{:?}", rc);
+    let thread = thread::spawn({
+        let counter = counter.clone();
+        move || {
+            for _ in 0..100000 {
+                let mut counter = counter.lock().unwrap();
 
-    let thread = thread::spawn(move || eprintln!("value = {}", rc));
+                if *counter % 2 == 0 {
+                    *counter += 1
+                }
+            }
+        }
+    });
+
+    for _ in 0..100000 {
+        let mut counter = counter.lock().unwrap();
+        if *counter % 2 == 1 {
+            *counter += 1
+        }
+    }
 
     thread.join().unwrap();
+
+    let counter = *counter.lock().unwrap();
+    println!("{:?}", counter);
 }
