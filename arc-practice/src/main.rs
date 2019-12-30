677bc39cf5a83618f089899458a1cbbbf89de382
Daichi Hioki <hiokidaichi@gmail.com>
2019-12-30 23:10:42 +0900

    Update

--- arc-practice/src/main.rs
+++ arc-practice/src/main.rs
@@ -1,10 +1,8 @@
-use std::rc::Rc;
-// use std::sync::Arc;
+use std::sync::Arc;
 use std::thread;
 
 fn main() {
-    // let rc = Arc::new(42);
-    let rc = Rc::new(42);
+    let rc = Arc::new(42);
 
     println!("{:?}", rc);
 
